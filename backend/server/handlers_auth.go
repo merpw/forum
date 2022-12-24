2b2d66c0cf5c7dbc00ae017a3d15dd3b0de1e079
tril <kyznector@gmail.com>
2022-12-24 19:31:52 +0200

    add error managing skeleton to logout handler

--- backend/server/handlers_auth.go
+++ backend/server/handlers_auth.go
@@ -1,6 +1,10 @@
 package server
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func (srv *Server) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	sendObject(w, "signup")
@@ -11,5 +15,34 @@ func (srv *Server) loginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (srv *Server) logoutHandler(w http.ResponseWriter, r *http.Request) {
+
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("ERROR %d. %v\n", http.StatusInternalServerError, err)
+			errorResponse(w, http.StatusInternalServerError) // 500 ERROR
+		}
+	}()
+
+	if r.URL.Path != "/api/logout" {
+		log.Printf("ERROR %d. r.URL.Path = %s != \"/api/logout\"\n", http.StatusNotFound, r.URL.Path)
+		errorResponse(w, http.StatusNotFound) // 404 ERROR
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		log.Printf("ERROR %d. ParseForm() err: %v\n", http.StatusBadRequest, err)
+		errorResponse(w, http.StatusBadRequest) // 400 ERROR
+		return
+	}
+
+	if r.Method != http.MethodPost { // not POST method case
+		log.Printf("ERROR %d. %v\n", http.StatusMethodNotAllowed, fmt.Sprintf("request method %s is inappropriate for the URL %s", r.Method, r.URL.Path))
+		errorResponse(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	// todo database stuff for logout + Error handling during fetching data
+
+	w.WriteHeader(http.StatusUnauthorized)
 	sendObject(w, "logout")
 }
