7d2dfecb540c32cb46ba2673c1185a9389e82bab
tril <kyznector@gmail.com>
2022-12-24 20:45:50 +0200

    move basic error handlig into handler_utils.go

--- backend/server/handlers_auth.go
+++ backend/server/handlers_auth.go
@@ -1,37 +1,12 @@
 package server
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 )
 
 func (srv *Server) signUpHandler(w http.ResponseWriter, r *http.Request) {
 
-	defer func() {
-		if err := recover(); err != nil {
-			log.Printf("ERROR %d. %v\n", http.StatusInternalServerError, err)
-			errorResponse(w, http.StatusInternalServerError) // 500 ERROR
-		}
-	}()
-
-	if r.URL.Path != "/api/signup" {
-		log.Printf("ERROR %d. r.URL.Path = %s != \"/api/signup\"\n", http.StatusNotFound, r.URL.Path)
-		errorResponse(w, http.StatusNotFound) // 404 ERROR
-		return
-	}
-
-	if err := r.ParseForm(); err != nil {
-		log.Printf("ERROR %d. ParseForm() err: %v\n", http.StatusBadRequest, err)
-		errorResponse(w, http.StatusBadRequest) // 400 ERROR
-		return
-	}
-
-	if r.Method != http.MethodPost { // not POST method case
-		log.Printf("ERROR %d. %v\n", http.StatusMethodNotAllowed, fmt.Sprintf("request method %s is inappropriate for the URL %s", r.Method, r.URL.Path))
-		errorResponse(w, http.StatusMethodNotAllowed)
-		return
-	}
+	errorBasicCheck(w, r, "/api/signup")
 
 	// todo database stuff for signup + Error handling during managing data
 
@@ -40,30 +15,7 @@ func (srv *Server) signUpHandler(w http.ResponseWriter, r *http.Request) {
 
 func (srv *Server) loginHandler(w http.ResponseWriter, r *http.Request) {
 
-	defer func() {
-		if err := recover(); err != nil {
-			log.Printf("ERROR %d. %v\n", http.StatusInternalServerError, err)
-			errorResponse(w, http.StatusInternalServerError) // 500 ERROR
-		}
-	}()
-
-	if r.URL.Path != "/api/login" {
-		log.Printf("ERROR %d. r.URL.Path = %s != \"/api/login\"\n", http.StatusNotFound, r.URL.Path)
-		errorResponse(w, http.StatusNotFound) // 404 ERROR
-		return
-	}
-
-	if err := r.ParseForm(); err != nil {
-		log.Printf("ERROR %d. ParseForm() err: %v\n", http.StatusBadRequest, err)
-		errorResponse(w, http.StatusBadRequest) // 400 ERROR
-		return
-	}
-
-	if r.Method != http.MethodPost { // not POST method case
-		log.Printf("ERROR %d. %v\n", http.StatusMethodNotAllowed, fmt.Sprintf("request method %s is inappropriate for the URL %s", r.Method, r.URL.Path))
-		errorResponse(w, http.StatusMethodNotAllowed)
-		return
-	}
+	errorBasicCheck(w, r, "/api/login")
 
 	// todo database stuff for login + Error handling during managing data
 
@@ -72,30 +24,7 @@ func (srv *Server) loginHandler(w http.ResponseWriter, r *http.Request) {
 
 func (srv *Server) logoutHandler(w http.ResponseWriter, r *http.Request) {
 
-	defer func() {
-		if err := recover(); err != nil {
-			log.Printf("ERROR %d. %v\n", http.StatusInternalServerError, err)
-			errorResponse(w, http.StatusInternalServerError) // 500 ERROR
-		}
-	}()
-
-	if r.URL.Path != "/api/logout" {
-		log.Printf("ERROR %d. r.URL.Path = %s != \"/api/logout\"\n", http.StatusNotFound, r.URL.Path)
-		errorResponse(w, http.StatusNotFound) // 404 ERROR
-		return
-	}
-
-	if err := r.ParseForm(); err != nil {
-		log.Printf("ERROR %d. ParseForm() err: %v\n", http.StatusBadRequest, err)
-		errorResponse(w, http.StatusBadRequest) // 400 ERROR
-		return
-	}
-
-	if r.Method != http.MethodPost { // not POST method case
-		log.Printf("ERROR %d. %v\n", http.StatusMethodNotAllowed, fmt.Sprintf("request method %s is inappropriate for the URL %s", r.Method, r.URL.Path))
-		errorResponse(w, http.StatusMethodNotAllowed)
-		return
-	}
+	errorBasicCheck(w, r, "/api/logout")
 
 	// todo database stuff for logout + Error handling during managing data
 
--- backend/server/handlers_utils.go
+++ backend/server/handlers_utils.go
@@ -12,6 +12,34 @@ func errorResponse(w http.ResponseWriter, code int) {
 	http.Error(w, fmt.Sprintf("%v %v", code, http.StatusText(code)), code)
 }
 
+// errorCheck handle basic request errors
+func errorBasicCheck(w http.ResponseWriter, r *http.Request, pathToCheck string) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("ERROR %d. %v\n", http.StatusInternalServerError, err)
+			errorResponse(w, http.StatusInternalServerError) // 500 ERROR
+		}
+	}()
+
+	if r.URL.Path != pathToCheck {
+		log.Printf("ERROR %d. r.URL.Path = %s != \""+pathToCheck+"\"\n", http.StatusNotFound, r.URL.Path)
+		errorResponse(w, http.StatusNotFound) // 404 ERROR
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		log.Printf("ERROR %d. ParseForm() err: %v\n", http.StatusBadRequest, err)
+		errorResponse(w, http.StatusBadRequest) // 400 ERROR
+		return
+	}
+
+	if r.Method != http.MethodPost { // not POST method case
+		log.Printf("ERROR %d. %v\n", http.StatusMethodNotAllowed, fmt.Sprintf("request method %s is inappropriate for the URL %s", r.Method, r.URL.Path))
+		errorResponse(w, http.StatusMethodNotAllowed)
+		return
+	}
+}
+
 // sendObject sends object to http.ResponseWriter
 //
 // calls errorResponse(500) if error happened
