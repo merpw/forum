f0087ea310ec9c8d48a25653d2413ac93547b9a9
tril <kyznector@gmail.com>
2022-12-24 19:50:52 +0200

    add skeleton for managing loginHandler errors

--- backend/server/handlers_auth.go
+++ backend/server/handlers_auth.go
@@ -11,6 +11,34 @@ func (srv *Server) signUpHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (srv *Server) loginHandler(w http.ResponseWriter, r *http.Request) {
+
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("ERROR %d. %v\n", http.StatusInternalServerError, err)
+			errorResponse(w, http.StatusInternalServerError) // 500 ERROR
+		}
+	}()
+
+	if r.URL.Path != "/api/login" {
+		log.Printf("ERROR %d. r.URL.Path = %s != \"/api/login\"\n", http.StatusNotFound, r.URL.Path)
+		errorResponse(w, http.StatusNotFound) // 404 ERROR
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		log.Printf("ERROR %d. ParseForm() err: %v\n", http.StatusBadRequest, err)
+		errorResponse(w, http.StatusBadRequest) // 400 ERROR
+		return
+	}
+
+	if r.Method != http.MethodPost { // not POST method case
+		log.Printf("ERROR %d. %v\n", http.StatusMethodNotAllowed, fmt.Sprintf("request method %s is inappropriate for the URL %s", r.Method, r.URL.Path))
+		errorResponse(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	// todo database stuff for login + Error handling during managing data
+
 	sendObject(w, "login")
 }
 
@@ -41,7 +69,7 @@ func (srv *Server) logoutHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// todo database stuff for logout + Error handling during fetching data
+	// todo database stuff for logout + Error handling during managing data
 
 	w.WriteHeader(http.StatusUnauthorized)
 	sendObject(w, "logout")
