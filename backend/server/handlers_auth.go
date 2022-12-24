b7894607ed10eb311fe1a635ecc2616ca0b2fe31
tril <kyznector@gmail.com>
2022-12-24 20:56:08 +0200

    refactor to use "errorBasicCheckPOST"

--- backend/server/handlers_auth.go
+++ backend/server/handlers_auth.go
@@ -8,7 +8,7 @@ func (srv *Server) signUpHandler(w http.ResponseWriter, r *http.Request) {
 
 	errorBasicCheckPOST(w, r, "/api/signup")
 
-	// todo database stuff for signup + Error handling during managing data
+	// todo database stuff for "signup" + Error handling during managing data
 
 	sendObject(w, "signup")
 }
@@ -17,7 +17,7 @@ func (srv *Server) loginHandler(w http.ResponseWriter, r *http.Request) {
 
 	errorBasicCheckPOST(w, r, "/api/login")
 
-	// todo database stuff for login + Error handling during managing data
+	// todo database stuff for "login" + Error handling during managing data
 
 	sendObject(w, "login")
 }
@@ -26,7 +26,7 @@ func (srv *Server) logoutHandler(w http.ResponseWriter, r *http.Request) {
 
 	errorBasicCheckPOST(w, r, "/api/logout")
 
-	// todo database stuff for logout + Error handling during managing data
+	// todo database stuff for "logout" + Error handling during managing data
 
 	w.WriteHeader(http.StatusUnauthorized)
 	sendObject(w, "logout")
