2e1d3ef552180e960cad9ccaed14f96dedb49977
Maksim Mikhailov <max@mer.pw>
2022-12-25 17:15:43 +0200

    Change max container width to 800px

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      screens: {
+        xl: "800px",
+        // TODO: remove when we will have more content to display (e.g. sidebar)
+      },
+    },
     extend: {},
   },
-  plugins: [],
 }
