4ea4d27fa51767171a340fb82d65a6067312a158
Maksim Mikhailov <max@mer.pw>
2022-12-26 21:56:11 +0200

    Add create new post form Add link to /me page

--- frontend/src/pages/create.tsx
+++ frontend/src/pages/create.tsx
@@ -1,21 +1,101 @@
+import { motion } from "framer-motion"
 import { NextPage } from "next"
+import Head from "next/head"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { useUser } from "../api/auth"
+import { CreatePost } from "../api/posts/create"
 
-const CreatePost: NextPage = () => {
-  const { isLoggedIn } = useUser()
+const CreatePostPage: NextPage = () => {
+  const { isLoading, isLoggedIn } = useUser()
   const router = useRouter()
 
   const [isRedirecting, setIsRedirecting] = useState(false) // Prevents duplicated redirects
   useEffect(() => {
-    if (isLoggedIn && !isRedirecting) {
+    if (!isLoading && !isLoggedIn && !isRedirecting) {
       setIsRedirecting(true)
       router.push("/login")
     }
-  }, [router, isLoggedIn, isRedirecting])
+  }, [router, isLoggedIn, isRedirecting, isLoading])
 
-  return <div>Create post</div>
+  return (
+    <>
+      <Head>
+        <title>Create new post - Forum</title>
+      </Head>
+      <CreatePostForm />
+    </>
+  )
 }
 
-export default CreatePost
+const CreatePostForm = () => {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  // const [categories, setCategories] = useState([])
+  const [formError, setFormError] = useState<string | null>(null)
+  const router = useRouter()
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        setFormError(null)
+        CreatePost(title, content)
+          .then((id) => {
+            router.push(`/post/${id}`)
+          })
+          .catch((err) => {
+            setFormError(err)
+          })
+      }}
+    >
+      <div className={"mb-3"}>
+        <input
+          type={"text"}
+          title={"title"}
+          className={
+            "bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 "
+          }
+          onInput={(e) => setTitle(e.currentTarget.value)}
+          placeholder={"Title"}
+          required
+        />
+      </div>
+      <div className={"mb-6"}>
+        <textarea
+          title={"content"}
+          className={
+            "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          }
+          onInput={(e) => setContent(e.currentTarget.value)}
+          placeholder={"Content"}
+          required
+        />
+      </div>
+      {formError != null && (
+        <motion.div
+          className={
+            "transition ease-in-out -translate-y-1 p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-inherit dark:border-2 dark:border-red-900 dark:text-white"
+          }
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 0.25 }}
+          exit={{ opacity: 0 }}
+          role={"alert"}
+        >
+          <span className={"font-medium"}>{formError}</span>
+        </motion.div>
+      )}
+      <button
+        type={"submit"}
+        className={
+          "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        }
+      >
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export default CreatePostPage
--- frontend/src/pages/me.tsx
+++ frontend/src/pages/me.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import Link from "next/link"
 import { useRouter } from "next/router"
 import { NextPage } from "next/types"
 import { useEffect, useState } from "react"
@@ -6,42 +7,73 @@ import { PostList } from "../components/posts"
 import { useUser } from "../api/auth"
 import { useUserPosts } from "../api/posts/fetch"
 
+/* TODO: add placeholders */
+
 const UserPage: NextPage = () => {
   const router = useRouter()
-  const { user, isError: isUserError, isLoading: isUserLoading, isLoggedIn } = useUser()
-  const { posts, isError: isPostsError, isLoading: isPostsLoading } = useUserPosts(user?.id)
+  const { isLoading, isLoggedIn } = useUser()
 
   const [isRedirecting, setIsRedirecting] = useState(false) // Prevents duplicated redirects
   useEffect(() => {
-    if (!isUserLoading && !isLoggedIn && !isRedirecting) {
+    if (!isLoading && !isLoggedIn && !isRedirecting) {
       setIsRedirecting(true)
       router.push("/login")
     }
-  }, [router, isLoggedIn, isRedirecting, isUserLoading])
-
-  if (isUserError) {
-    return <div>Error</div>
-  }
-
-  if (isUserLoading) {
-    return <div>Loading...</div>
-  }
+  }, [router, isLoggedIn, isRedirecting, isLoading])
 
   return (
     <>
       <Head>
         <title>{`Profile - Forum`}</title>
       </Head>
-      <h1 className={"text-2xl font-thin mb-5"}>
-        {"Hello, "}
-        <span className={"text-3xl font-normal"}>{user?.name}</span>
-      </h1>
-      <h2 className={"text-2xl mb-3"}>Your recent posts:</h2>
-      {isPostsLoading && <div>Loading...</div>}
-      {isPostsError && <div>Error</div>}
-      {posts && <PostList posts={posts} />}
+      <UserInfo />
+      <Link href={"/create"} className={"text-2xl hover:opacity-50 mb-5 flex gap-1"}>
+        <span className={"my-auto"}>
+          <svg
+            xmlns={"http://www.w3.org/2000/svg"}
+            fill={"none"}
+            viewBox={"0 0 24 24"}
+            strokeWidth={1.5}
+            stroke={"currentColor"}
+            className={"w-6 h-6 "}
+          >
+            <path
+              strokeLinecap={"round"}
+              strokeLinejoin={"round"}
+              d={
+                "M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+              }
+            />
+          </svg>
+        </span>
+        <span>Create a new post</span>
+      </Link>
+      <h2 className={"text-xl mb-3"}>Your recent posts:</h2>
+      <UserPosts />
     </>
   )
+}
+
+const UserInfo = () => {
+  const { user } = useUser()
+
+  return (
+    <h1 className={"text-2xl font-thin mb-5"}>
+      {"Hello, "}
+      <span className={"text-3xl font-normal"}>{user?.name}</span>
+    </h1>
+  )
+}
+
+const UserPosts = () => {
+  const { user } = useUser()
+  const { posts } = useUserPosts(user?.id)
+
+  if (posts == undefined) return null
+
+  if (posts.length == 0) return <div>{"You haven't posted yet"}</div>
+
+  return <PostList posts={posts} />
 }
 
 export default UserPage
