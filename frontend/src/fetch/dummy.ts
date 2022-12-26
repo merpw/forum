cb3f683f5763f88f89227e85f4b7351d3ffdd5b5
Maksim Mikhailov <max@mer.pw>
2022-12-26 16:49:49 +0200

    Change user ids in dummy data

--- frontend/src/fetch/dummy.ts
+++ frontend/src/fetch/dummy.ts
@@ -6,11 +6,11 @@ export const user = {
   email: "max@mer.pw",
 }
 
-export const users: User[] = [user, { name: "Cat", id: 1 }]
+export const users: User[] = [user, { name: "Cat", id: 2 }]
 
 export const posts: Post[] = [
   {
-    id: 0,
+    id: 1,
     title: "Post 1",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam euismod imperdiet dictum. Sed aliquam lacus neque, in efficitur diam ultrices quis. Nam viverra varius quam, a dictum purus congue eu. Vivamus at vehicula massa. Pellentesque non eros augue. Praesent ut ante quis dui hendrerit iaculis ut pretium velit. Vivamus sed ante aliquam, accumsan ipsum nec, suscipit lorem. Ut eu diam nec mi mattis imperdiet. Vivamus massa est, cursus vitae felis sed, congue semper lectus. Proin placerat gravida nisi, malesuada pulvinar ligula semper non. Sed rhoncus augue id tempus congue. In laoreet a mi nec pharetra. Praesent tempor erat et eros bibendum porttitor at et erat. Proin aliquet pellentesque lacus, non ullamcorper mi placerat pulvinar.",
@@ -32,11 +32,11 @@ export const posts: Post[] = [
     ],
   },
   {
-    id: 1,
+    id: 2,
     title: "Post 2",
     content:
       "Aenean non semper sapien. Sed at tristique sapien. Etiam auctor accumsan mi, a lobortis enim sollicitudin a. Aliquam pellentesque ligula ullamcorper egestas tempor. Curabitur congue nec odio ut ultricies. Donec tempus tincidunt mi non bibendum. Sed quis malesuada ex. Mauris gravida luctus interdum. Vivamus leo ligula, scelerisque sit amet arcu ac, rhoncus rutrum augue. Integer congue magna sem, sit amet euismod lacus blandit sit amet. Maecenas ultrices odio ipsum, eget imperdiet nibh tempus a. Donec neque diam, sodales eget aliquam vel, fringilla nec eros. Proin tincidunt felis arcu, a tempus ante hendrerit eget. Vestibulum eget nisi eget tellus porttitor interdum a nec velit.",
-    author: { id: 1, name: "Cat" },
+    author: users[1],
     date: "2022-12-22T19:36:18.166Z",
     likes: 0,
     dislikes: 0,
