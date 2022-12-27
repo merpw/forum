ce4ea53bc11acce05c2ff6f842ddf35781ada4b8
tril <kyznector@gmail.com>
2022-12-28 01:43:12 +0200

    remove artefacts

--- backend/server/handlers_utils.go
+++ backend/server/handlers_utils.go
@@ -12,14 +12,6 @@ func errorResponse(w http.ResponseWriter, code int) {
 	http.Error(w, fmt.Sprintf("%v %v", code, http.StatusText(code)), code)
 }
 
-func (srv *Server) methodGetHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func (srv *Server) methodPostHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
 // sendObject sends object to http.ResponseWriter
 //
 // calls errorResponse(500) if error happened
