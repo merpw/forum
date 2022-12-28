125092bbc4e0ee6f0e6a082fd0bbd891ece37053
sagar yadav <5121817+sagarishere@users.noreply.github.com>
2022-12-28 03:41:31 +0200

    improved a comment on a function, on how to use it

--- backend/database/openDb.go
+++ backend/database/openDb.go
@@ -8,6 +8,10 @@ import (
 )
 
 // return a pointer to a sql.DB object
+//
+// how to use:
+// db := Opendb()
+// Special note. No need to defer close a connection as it is automatically closed by sql driver
 func Opendb() *sql.DB {
 	db, err := sql.Open("sqlite3", "./database/databsase.db")
 	if err != nil {
@@ -15,9 +19,3 @@ func Opendb() *sql.DB {
 	}
 	return db
 }
-
-// how to use the Opendb function
-// db, err := Opendb()
-// if err != nil {
-// 	log.Fatal(err)
-// }
