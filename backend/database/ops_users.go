e2c84b732d0a53eb5c0a422d056a4d10a4698820
tril <kyznector@gmail.com>
2022-12-30 15:34:31 +0200

    add `(db DB) IsNameTaken`

--- backend/database/ops_users.go
+++ backend/database/ops_users.go
@@ -75,3 +75,15 @@ func (db DB) IsEmailTaken(email string) bool {
 }
 
 // TODO: maybe add IsNameTaken
+// IsUserTaken checks if name is already in use.
+// Added because authorization supports the name and the email as login
+func (db DB) IsNameTaken(name string) bool {
+	query, err := db.Query("SELECT 1 FROM users WHERE name = ?", name)
+	if err != nil {
+		log.Panic(err)
+	}
+	isTaken := query.Next()
+	query.Close()
+
+	return isTaken
+}
