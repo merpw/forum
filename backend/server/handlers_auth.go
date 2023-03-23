154605141c48ddfec9ccbd0a23667eeb99aba4d8
Maksim Mikhailov <max@mer.pw>
2023-03-23 16:59:15 +0200

    [backend] bad commit, signup destroyed

--- backend/server/handlers_auth.go
+++ backend/server/handlers_auth.go
@@ -3,7 +3,6 @@ package server
 import (
 	"encoding/json"
 	"net/http"
-	"net/mail"
 	"strings"
 	"time"
 
@@ -12,63 +11,63 @@ import (
 )
 
 func (srv *Server) signupHandler(w http.ResponseWriter, r *http.Request) {
-	if srv.getUserId(w, r) != -1 {
-		http.Error(w, "You are already logged in", http.StatusBadRequest)
-		return
-	}
-
-	requestBody := struct {
-		Name     string `json:"name"`
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}{}
-	err := json.NewDecoder(r.Body).Decode(&requestBody)
-	if err != nil {
-		http.Error(w, "Body is not valid", http.StatusBadRequest)
-		return
-	}
-	requestBody.Email = strings.ToLower(requestBody.Email)
-	if len(requestBody.Name) < 3 {
-		http.Error(w, "Name is too short", http.StatusBadRequest)
-		return
-	}
-	if len(requestBody.Name) > 15 {
-		http.Error(w, "Name is too long", http.StatusBadRequest)
-		return
-	}
-	if requestBody.Name != strings.TrimSpace(requestBody.Name) {
-		http.Error(w, "Name is not valid", http.StatusBadRequest)
-		return
-	}
-	// here is checking name is already in use,
-	// statement above checks the incoming name do not include spaces before and after
-	if srv.DB.IsNameTaken(requestBody.Name) {
-		http.Error(w, "Name is already in use", http.StatusBadRequest)
-		return
-	}
-	// check if email is a valid email
-	_, err = mail.ParseAddress(requestBody.Email)
-	if err != nil || requestBody.Email != strings.TrimSpace(requestBody.Email) {
-		http.Error(w, "Email is not valid", http.StatusBadRequest)
-		return
-	}
-
-	if srv.DB.IsEmailTaken(requestBody.Email) {
-		http.Error(w, "Email is already taken", http.StatusBadRequest)
-		return
-	}
-
-	if len(requestBody.Password) < 8 {
-		http.Error(w, "Password is too short", http.StatusBadRequest)
-		return
-	}
-
-	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(requestBody.Password), 10)
-	if err != nil {
-		http.Error(w, "Password is not valid", http.StatusBadRequest)
-		return
-	}
-	srv.DB.AddUser(requestBody.Name, requestBody.Email, string(encryptedPassword))
+	//if srv.getUserId(w, r) != -1 {
+	//	http.Error(w, "You are already logged in", http.StatusBadRequest)
+	//	return
+	//}
+	//
+	//requestBody := struct {
+	//	Name     string `json:"name"`
+	//	Email    string `json:"email"`
+	//	Password string `json:"password"`
+	//}{}
+	//err := json.NewDecoder(r.Body).Decode(&requestBody)
+	//if err != nil {
+	//	http.Error(w, "Body is not valid", http.StatusBadRequest)
+	//	return
+	//}
+	//requestBody.Email = strings.ToLower(requestBody.Email)
+	//if len(requestBody.Name) < 3 {
+	//	http.Error(w, "Name is too short", http.StatusBadRequest)
+	//	return
+	//}
+	//if len(requestBody.Name) > 15 {
+	//	http.Error(w, "Name is too long", http.StatusBadRequest)
+	//	return
+	//}
+	//if requestBody.Name != strings.TrimSpace(requestBody.Name) {
+	//	http.Error(w, "Name is not valid", http.StatusBadRequest)
+	//	return
+	//}
+	//// here is checking name is already in use,
+	//// statement above checks the incoming name do not include spaces before and after
+	//if srv.DB.IsNameTaken(requestBody.Name) {
+	//	http.Error(w, "Name is already in use", http.StatusBadRequest)
+	//	return
+	//}
+	//// check if email is a valid email
+	//_, err = mail.ParseAddress(requestBody.Email)
+	//if err != nil || requestBody.Email != strings.TrimSpace(requestBody.Email) {
+	//	http.Error(w, "Email is not valid", http.StatusBadRequest)
+	//	return
+	//}
+	//
+	//if srv.DB.IsEmailTaken(requestBody.Email) {
+	//	http.Error(w, "Email is already taken", http.StatusBadRequest)
+	//	return
+	//}
+	//
+	//if len(requestBody.Password) < 8 {
+	//	http.Error(w, "Password is too short", http.StatusBadRequest)
+	//	return
+	//}
+	//
+	//encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(requestBody.Password), 10)
+	//if err != nil {
+	//	http.Error(w, "Password is not valid", http.StatusBadRequest)
+	//	return
+	//}
+	//srv.DB.AddUser(requestBody.Name, requestBody.Email, string(encryptedPassword))
 }
 
 func (srv *Server) loginHandler(w http.ResponseWriter, r *http.Request) {
