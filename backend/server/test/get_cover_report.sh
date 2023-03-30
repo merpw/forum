bb3feb7dd136ead304117b6b4999ac7b763aae7a
sagarishere <5121817+sagarishere@users.noreply.github.com>
2023-03-30 21:02:44 +0300

    Add folder cover_reports mkdir in bash cover test file

--- backend/server/test/get_cover_report.sh
+++ backend/server/test/get_cover_report.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# make folder cover_reports if it doesn't exist
+mkdir -p ./cover_reports
+
 # Run unit tests for forum/server/test package with coverage analysis enabled
 go test "forum/server/test" -cover -coverpkg="../../..." -coverprofile="./cover_reports/profile.txt"
 
