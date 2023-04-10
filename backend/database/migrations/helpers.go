92bec24a69b4748bd1493c693235353b15822fca
Maksim Mikhailov <max@mer.pw>
2023-04-10 20:03:04 +0300

    [backend] Add database migrations
    Implement basic package for revision management
    
    Add initial revision with current database structure
    
    Change InitDatabase to use new migrations package

---
+++ backend/database/migrations/helpers.go
@@ -0,0 +1,22 @@
+package migrations
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+// GetVersion returns the current database version (user_version pragma)
+func GetVersion(db *sql.DB) (int, error) {
+	var version int
+	err := db.QueryRow("PRAGMA user_version").Scan(&version)
+	if err != nil {
+		return 0, err
+	}
+	return version, nil
+}
+
+// SetVersion sets the current database version (user_version pragma)
+func SetVersion(db *sql.DB, version int) error {
+	_, err := db.Exec(fmt.Sprintf("PRAGMA user_version = %d", version))
+	return err
+}
---
+++ backend/database/migrations/migrate.go
@@ -0,0 +1,66 @@
+package migrations
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type Migration struct {
+	Up   func(db *sql.DB) error
+	Down func(db *sql.DB) error
+}
+
+var Migrations = []Migration{
+	v001,
+}
+
+// Migrate migrates the database to the specified revision
+//
+// If the database is empty, it will create the initial schema
+func Migrate(db *sql.DB, toRevision int) error {
+	dbRevision, err := GetVersion(db)
+	if err != nil {
+		return err
+	}
+
+	if dbRevision == toRevision {
+		return nil
+	}
+
+	if toRevision > len(Migrations) || toRevision < 1 {
+		return fmt.Errorf("invalid revision %d, availible revisions 1..%d", toRevision, len(Migrations))
+	}
+
+	if dbRevision == 0 {
+		log.Printf("Empty/unknown database, initial schema will be created")
+	}
+
+	if dbRevision > toRevision {
+		log.Printf("Migrating database down from revision %d to %d", dbRevision, toRevision)
+		for i := dbRevision; i > toRevision; i-- {
+			err := Migrations[i-1].Down(db)
+			if err != nil {
+				return err
+			}
+			err = SetVersion(db, i-1)
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		log.Printf("Migrating database up from revision %d to %d", dbRevision, toRevision)
+		for i := dbRevision; i < toRevision; i++ {
+			err := Migrations[i].Up(db)
+			if err != nil {
+				return err
+			}
+			err = SetVersion(db, i+1)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
