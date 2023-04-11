4ca1100a63242f2b35bc53e843b4eb006d838b2a
natkim <natalia.kim.masseli@gmail.com>
2023-04-11 19:46:26 +0300

    refactor new fields: First name, Last name, Date of Birth, Gender
    rename Age param to DoB (Date of Birth)
    change DoB type from Text to Date
    change input element for Gender to Select (male, female, other)
    change new fields status to required
    align it by two in a row
    wrap input, select elements inside label elem. for better accessibility

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -17,7 +17,7 @@ const SignupPage: NextPage = () => {
   const [passwordConfirm, setPasswordConfirm] = useState("")
   const [first_name, setFirstName] = useState("")
   const [last_name, setLastName] = useState("")
-  const [age, setAge] = useState("")
+  const [dob, setDoB] = useState("")
   const [gender, setGender] = useState("")
 
   const [isSame, setIsSame] = useState(false)
@@ -34,7 +34,7 @@ const SignupPage: NextPage = () => {
 
   useEffect(() => {
     setIsSame(false)
-  }, [name, email, password, passwordConfirm, first_name, last_name, age, gender])
+  }, [name, email, password, passwordConfirm, first_name, last_name, dob, gender])
 
   return (
     <>
@@ -57,7 +57,7 @@ const SignupPage: NextPage = () => {
             return
           }
 
-          SignUp(name, email, password, first_name, last_name, age, gender)
+          SignUp(name, email, password, first_name, last_name, dob, gender)
             .then((response) => {
               if (response.status == 200) {
                 logIn(email, password).then(() => mutate())
@@ -78,106 +78,110 @@ const SignupPage: NextPage = () => {
             className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
           >
             Username
+            <input
+              type={"text"}
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              }
+              onInput={(e) => setName(e.currentTarget.value)}
+              placeholder={"Username"}
+              required
+            />
           </label>
-          <input
-            type={"text"}
-            id={"username"}
-            className={
-              "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            }
-            onInput={(e) => setName(e.currentTarget.value)}
-            placeholder={"Username"}
-            required
-          />
         </div>
-
-        <div className={"mb-6"}>
-          <label
-            htmlFor={"first_name"}
-            className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
-          >
-            First Name
-          </label>
-          <input
-            type={"text"}
-            id={"first_name"}
-            className={
-              "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            }
-            onInput={(e) => setFirstName(e.currentTarget.value)}
-            placeholder={"First Name"}
-          />
+        <div className={"flex flex-raw mb-3 gap-3"}>
+          <div className={"w-full"}>
+            <label
+              htmlFor={"first_name"}
+              className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
+            >
+              First Name
+              <input
+                type={"text"}
+                className={
+                  "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                }
+                onInput={(e) => setFirstName(e.currentTarget.value)}
+                placeholder={"First Name"}
+                required
+              />
+            </label>
+          </div>
+          <div className={"w-full"}>
+            <label
+              htmlFor={"last_name"}
+              className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
+            >
+              Last Name
+              <input
+                type={"text"}
+                className={
+                  "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                }
+                onInput={(e) => setLastName(e.currentTarget.value)}
+                placeholder={"Last Name"}
+                required
+              />
+            </label>
+          </div>
         </div>
-        <div className={"mb-6"}>
-          <label
-            htmlFor={"last_name"}
-            className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
-          >
-            Last Name
-          </label>
-          <input
-            type={"text"}
-            id={"last_name"}
-            className={
-              "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            }
-            onInput={(e) => setLastName(e.currentTarget.value)}
-            placeholder={"Last Name"}
-          />
+        <div className={"flex flex-raw mb-6 gap-3"}>
+          <div className={"w-full flex-1 overflow-x-hidden"}>
+            <label
+              htmlFor={"dob"}
+              className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
+            >
+              Date of Birth
+              <input
+                type={"date"}
+                max={new Date().toISOString().split("T")[0]}
+                className={
+                  "h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                }
+                onInput={(e) => setDoB(e.currentTarget.value)}
+                placeholder={"Date of Birth"}
+                required
+              />
+            </label>
+          </div>
+          <div className={"w-full flex-1"}>
+            <label
+              htmlFor={"gender"}
+              className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
+            >
+              Gender
+              <select
+                className={
+                  "h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                }
+                onInput={(e) => setGender(e.currentTarget.value)}
+                placeholder={"Gender"}
+                required
+              >
+                <option value={""}>Select</option>
+                <option value={"male"}>Male</option>
+                <option value={"female"}>Female</option>
+                <option value={"other"}>Other</option>
+              </select>
+            </label>
+          </div>
         </div>
-        <div className={"mb-6"}>
-          <label
-            htmlFor={"age"}
-            className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
-          >
-            Age
-          </label>
-          <input
-            type={"number"}
-            min={0}
-            id={"age"}
-            className={
-              "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            }
-            onInput={(e) => setAge(e.currentTarget.value)}
-            placeholder={"Age"}
-          />
-        </div>
-        <div className={"mb-6"}>
-          <label
-            htmlFor={"gender"}
-            className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
-          >
-            Gender
-          </label>
-          <input
-            type={"text"}
-            id={"gender"}
-            className={
-              "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            }
-            onInput={(e) => setGender(e.currentTarget.value)}
-            placeholder={"Gender"}
-          />
-        </div>
-
         <div className={"mb-6"}>
           <label
             htmlFor={"email"}
             className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
           >
             Your email
+            <input
+              type={"email"}
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              }
+              onInput={(e) => setEmail(e.currentTarget.value)}
+              placeholder={"Email"}
+              required
+            />
           </label>
-          <input
-            type={"email"}
-            id={"email"}
-            className={
-              "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            }
-            onInput={(e) => setEmail(e.currentTarget.value)}
-            placeholder={"Email"}
-            required
-          />
         </div>
         <div className={"mb-3"}>
           <label
@@ -185,16 +189,15 @@ const SignupPage: NextPage = () => {
             className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
           >
             Create password
+            <input
+              onInput={(e) => setPassword(e.currentTarget.value)}
+              type={"password"}
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              }
+              required
+            />
           </label>
-          <input
-            onInput={(e) => setPassword(e.currentTarget.value)}
-            type={"password"}
-            id={"password"}
-            className={
-              "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            }
-            required
-          />
         </div>
         <div className={"mb-6"}>
           <label
