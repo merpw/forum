64173a71e738cc6bd14135ba3b011f4e0df22dfb
Maksim Mikhailov <43277901+maximihajlov@users.noreply.github.com>
2023-04-12 21:42:25 +0300

    Features/database migrations (#22)
    * [backend] Add database migrations
    
    Add new package `migrate` to handle migrations functionality 
    
    Add initial revision with the current database structure
    
    Add first migration to make all fields NOT NULL
    
    Implement CLI for manual migrations
    
    Add tests for migrate package with 80% coverage
    
    Improve documentation, add info about migrations

---
+++ backend/database/migrate/migrations/migrations.go
@@ -0,0 +1,15 @@
+package migrations
+
+import "database/sql"
+
+// Migration defines operations that should be done to move Up or Down between revisions
+type Migration struct {
+	Up   func(db *sql.DB) error
+	Down func(db *sql.DB) error
+}
+
+// Migrations defines all available Migrations and their order
+var Migrations = []Migration{
+	v001,
+	v002,
+}
