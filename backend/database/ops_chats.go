ddbe4f307dc0e1d6810fc9139dd3f7735b828ee5
healingdrawing <healingdrawing@gmail.com>
2023-04-17 00:11:17 +0300

    Add chat UI list queries. Tests not implemented.
    OnlineUser struct includes Id and Name of user.
    
    GetOnlineUsersIdsAndNames() - to get online users names and ids.
    
    GetPrivateChatOponentsByUserId() - to get already exists private chats.

--- backend/database/ops_chats.go
+++ backend/database/ops_chats.go
@@ -172,3 +172,71 @@ func (db DB) AddMessage(userId, chatId int, content string) int {
 	}
 	return int(id)
 }
+
+// TODO: perhaps move it later to models.go. Not approved yet.
+type OnlineUser struct {
+	Id   int
+	Name string
+}
+
+/*
+GetOnlineUsersIdsAndNames reads online users from database, does not require user to be logged in.
+
+	Use this to get the list of online users for a user side panel with available users to chat with.
+*/
+func (db DB) GetOnlineUsersIdsAndNames(userId int) []OnlineUser {
+	query, err := db.Query(
+		"SELECT users.id, users.name FROM users "+
+			"JOIN sessions ON sessions.user_id = users.id "+
+			"WHERE users.id != ?",
+		userId)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var onlineUsers []OnlineUser
+	for query.Next() {
+		var onlineUser OnlineUser
+		err = query.Scan(&onlineUser.Id, &onlineUser.Name)
+		if err != nil {
+			log.Panic(err)
+		}
+		onlineUsers = append(onlineUsers, onlineUser)
+	}
+	query.Close()
+
+	return onlineUsers
+}
+
+// GetPrivateChatOponentsByUserId reads private chat oponents from database by user_id
+func (db DB) GetPrivateChatOponentsByUserId(userId int) []OnlineUser {
+	// requirements:
+	// chat.type = 2 to get private chats,
+	// users.id != userId to get oponents, not the user itself
+	// o = oponent, u = user
+	// TODO: check this properly. It looks not clear. But at the moment there is no data and tests to check it.
+	query, err := db.Query(
+		"SELECT o.id, o.name FROM users AS o "+
+			"JOIN memberships AS om ON om.user_id = o.id "+
+			"JOIN chats AS c ON c.id = om.chat_id "+
+			"JOIN memberships AS um ON um.chat_id = c.id "+
+			"JOIN users AS u ON u.id = um.user_id "+
+			"WHERE c.type = 2 AND o.id != ? AND u.id = ?",
+		userId)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var oponents []OnlineUser
+	for query.Next() {
+		var oponent OnlineUser
+		err = query.Scan(&oponent.Id, &oponent.Name)
+		if err != nil {
+			log.Panic(err)
+		}
+		oponents = append(oponents, oponent)
+	}
+	query.Close()
+
+	return oponents
+}
