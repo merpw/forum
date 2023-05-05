243b62168a8a05d5e8c471bd13ca291e57e96568
Maksim Mikhailov <max@mer.pw>
2023-05-05 18:01:58 +0300

    [frontend] Add Chat button on user page

--- frontend/src/pages/user/[id].tsx
+++ frontend/src/pages/user/[id].tsx
@@ -1,14 +1,16 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { FC, useEffect, useRef, useState } from "react"
 import { NextSeo } from "next-seo"
 import { AxiosError } from "axios"
+import Link from "next/link"
 
 import { Post, User } from "@/custom"
 import { getUserLocal } from "@/api/users/fetch"
 import { useMe } from "@/api/auth"
 import { getUserPostsLocal } from "@/api/posts/fetch"
 import { PostList } from "@/components/posts/list"
+import { useCreateChat, useUserChat } from "@/api/chats/chats"
 
 const UserPage: NextPage<{ user: User; posts: Post[] }> = ({ user, posts }) => {
   const router = useRouter()
@@ -27,13 +29,47 @@ const UserPage: NextPage<{ user: User; posts: Post[] }> = ({ user, posts }) => {
     <>
       <NextSeo title={user.name} description={`Posts created by ${user.name}`} />
       <div>
-        <h1 className={"text-2xl mb-5"}>{user.name}</h1>
+        <h1 className={"text-2xl mb-3"}>{user.name}</h1>
+        <ChatButton userId={user.id} />
         <div>
           <h2 className={"text-xl mb-2"}>Recent posts:</h2>
           <PostList posts={posts.sort((a, b) => b.date.localeCompare(a.date))} />
         </div>
       </div>
     </>
+  )
+}
+
+const ChatButton: FC<{ userId: number }> = ({ userId }) => {
+  const { chatId } = useUserChat(userId)
+  const wasNull = useRef(false)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (chatId === null) wasNull.current = true
+    else if (wasNull.current) router.push(`/chat/${chatId}`)
+  }, [chatId, router])
+
+  return (
+    <>
+      {chatId !== null ? (
+        <Link href={`/chat/${chatId}`} className={"clickable text-2xl mb-3"}>
+          Open chat
+        </Link>
+      ) : (
+        <CreateChatButton userId={userId} />
+      )}
+    </>
+  )
+}
+
+const CreateChatButton: FC<{ userId: number }> = ({ userId }) => {
+  const createChat = useCreateChat()
+  return (
+    <button className={"clickable text-2xl mb-3"} onClick={() => createChat(userId)}>
+      Create chat
+    </button>
   )
 }
 
