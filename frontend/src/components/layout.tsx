64f049e03aad8e1c4d392f04e4f5ddfac290cd3f
Maksim Mikhailov <max@mer.pw>
2023-05-05 18:00:41 +0300

    [frontend] Add the Chat section
    Add ChatList component to show list of user's chats
    
    Add ChatList to lay out to show this section on all pages
    
    Do not show ChatList if user is not logged in
    
    Auto-show ChatList on desktop devices

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,27 +1,82 @@
-import { ReactNode } from "react"
+"use client"
+
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react"
+import { Provider } from "react-redux"
 import Link from "next/link"
 
-import Navbar from "./navbar"
+import { useMe } from "@/api/auth"
+import Navbar from "@/components/navbar"
+import store from "@/store/store"
+import ChatsSection from "@/components/ChatSection"
+
+export const ChatSectionCollapsedContext = createContext({
+  isCollapsed: false,
+  setIsCollapsed: (() => void 0) as Dispatch<SetStateAction<boolean>>,
+})
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const { user } = useMe()
+  const scrollRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    // TODO: add scroll restoration
+    if (scrollRef.current) {
+      scrollRef.current.scrollTo(0, 0)
+    }
+  }, [children])
+
+  const [isCollapsed, setIsCollapsed] = useState(true)
+
+  useEffect(() => {
+    if (window.innerWidth >= 640) {
+      setIsCollapsed(false)
+    }
+  }, [])
+
   return (
-    <div className={"container m-auto p-5 min-h-screen break-words"}>
-      <header>
-        <Navbar />
-      </header>
-      <main>{children}</main>
-      <footer>
-        <div className={"text-center font-light text-neutral border-t border-base-100 pt-3 mt-3"}>
-          <p>
-            {"Made with "}
-            <Link href={"https://github.com/merpw/forum"} className={"gradient-text clickable"}>
-              {"❤"}
-            </Link>
-            {" by "}
-            <span className={"font-light"}>{"basket of kittens"}</span>
-          </p>
+    <Provider store={store}>
+      <div className={"container m-auto p-5 min-h-screen break-words"}>
+        <header>
+          <Navbar />
+        </header>
+        <div className={"mx-5 flex gap-3 flex-row h-[calc(100vh-5.5rem)] relative"}>
+          <main
+            ref={scrollRef}
+            className={
+              "max-h-full overflow-auto grow" +
+              " " +
+              (user && !isCollapsed ? "hidden sm:block w-3/4" : "")
+            }
+          >
+            {children}
+          </main>
+          {user && (
+            <ChatSectionCollapsedContext.Provider value={{ isCollapsed, setIsCollapsed }}>
+              <ChatsSection />
+            </ChatSectionCollapsedContext.Provider>
+          )}
         </div>
-      </footer>
-    </div>
+        <footer>
+          <div className={"text-center font-light text-neutral border-t border-base-100 pt-3 mt-3"}>
+            <p>
+              {"Made with "}
+              <Link href={"https://github.com/merpw/forum"} className={"gradient-text clickable"}>
+                {"❤"}
+              </Link>
+              {" by "}
+              <span className={"font-light"}>{"basket of kittens"}</span>
+            </p>
+          </div>
+        </footer>
+      </div>
+    </Provider>
   )
 }
