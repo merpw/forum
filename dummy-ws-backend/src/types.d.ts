50143be7e0b219ee339db90a169cc47cd442e22c
Maksim Mikhailov <max@mer.pw>
2023-05-05 15:41:01 +0300

    Add dummy WebSocket backend
    Implement all basic messages types and endpoints
    
    Add a short documentation in README and add link to Postman workspace

---
+++ dummy-ws-backend/src/types.d.ts
@@ -0,0 +1,78 @@
+export type Message = {
+  id: number
+  chatId: number
+  content: string
+  // -1 for system messages
+  authorId: number
+  timestamp: string
+}
+
+export type Chat = {
+  id: number
+  lastMessageId: number
+  userId: number
+}
+
+export type WSBase = {
+  type: string
+  item: object
+}
+
+export type WSHandshake = WSBase & {
+  type: "handshake"
+  item: {
+    token: string
+  }
+}
+
+// Get requests and responses
+
+export type WSGetRequest = WSBase & {
+  type: "get"
+  item: {
+    url: string
+  }
+}
+
+export type WSGetResponse<T> = WSBase & {
+  type: "get"
+  item: {
+    url: string
+    data: T
+  }
+}
+
+//
+
+// Post requests and responses
+
+export type WSPostRequest<T> = WSBase & {
+  type: "post"
+  item: {
+    url: string
+    data: T
+  }
+}
+
+export type WSPostResponse<T> = WSBase & {
+  type: "post"
+  item: {
+    url: string
+    data: T
+  }
+}
+
+export type WSErrorResponse = WSBase & {
+  type: "error"
+  item: {
+    message: string
+  }
+}
+
+export type WebSocketRequest<T> = WSHandshake | WSGetRequest | WSPostRequest<T>
+
+export type WebSocketResponse<T> =
+  | WSHandshake
+  | WSGetResponse<T>
+  | WSPostResponse<T>
+  | WSErrorResponse
