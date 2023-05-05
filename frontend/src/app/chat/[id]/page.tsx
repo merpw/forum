a8cd21fb8458d82c2dbd5284f17a070ef31e0cab
Maksim Mikhailov <max@mer.pw>
2023-05-05 18:01:29 +0300

    [frontend] Add Chat page

---
+++ frontend/src/app/chat/[id]/page.tsx
@@ -0,0 +1,7 @@
+import ChatPage from "@/app/chat/[id]/chat-page"
+
+const Page = () => {
+  return <ChatPage />
+}
+
+export default Page
---
+++ frontend/src/app/chat/page.tsx
@@ -0,0 +1,6 @@
+const ChatPage = () => {
+  // TODO: maybe show a list of chats here?
+  return <div>No chat selected</div>
+}
+
+export default ChatPage
