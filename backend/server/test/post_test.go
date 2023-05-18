89c9ba6b382e94e2d2a1bc6433214d6186fa8d08
sagarishere <5121817+sagarishere@users.noreply.github.com>
2023-05-18 18:33:57 +0300

    Add: dummylogin to post_test.go
    dummyLogin function added to post_test.go for better organization of tests.

--- backend/server/test/post_test.go
+++ backend/server/test/post_test.go
@@ -2,11 +2,14 @@ package server_test
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"forum/server"
 	"io"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 	"time"
@@ -14,10 +17,142 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+// cookie to simulate logged-in user.
+var cookie *http.Cookie
+
+type TestPost struct {
+	Title      string
+	Content    string
+	Categories []string
+}
+
+var invalidPosts = []TestPost{
+	{
+		Title:      "",
+		Content:    "Valid Content",
+		Categories: []string{"facts"},
+	},
+	{
+		Title:      "invalidTitleTooLongItWillExceed25length",
+		Content:    "Valid Content",
+		Categories: []string{"facts"},
+	},
+	{
+		Title:      "Valid Title",
+		Content:    "",
+		Categories: []string{"facts"},
+	},
+	{
+		Title:      "Valid Title",
+		Content:    "Valid title",
+		Categories: []string{"Invalid category"},
+	},
+}
+
+// TestUser to be used in all Post tests.
+type TestUser struct {
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	DoB       string `json:"dob"`
+	Gender    string `json:"gender"`
+}
+
+var invalidUsers = []TestUser{
+	{
+		Name:     "test1",
+		Email:    "test@test.com", // email already in use
+		Password: "SuperAmazingPassword()!@*#)(!@#",
+	},
+	{
+		Name:     "",
+		Email:    "",
+		Password: "",
+	},
+	{
+		Name:     "ThisUserNameIsWayTooLong",
+		Email:    "valid@test.com",
+		Password: "ValidPassword123",
+	},
+	{
+		Name:     "ValidName",
+		Email:    "😂😂😂😂😂😂😂😂😂😂😂", // invalid email
+		Password: "ValidPassword123",
+	},
+	{
+		Name:     "ValidName",
+		Email:    "valid@test.com",
+		Password: "1234", // invalid password: too short
+	},
+	{
+		Name:     "Steve", // Invalid name: already in use
+		Email:    "valid@test.com",
+		Password: "12345678",
+	},
+	{
+		Name:     "ValidName",
+		Email:    "steve@apple.com", // Invalid e-mail: Already in use
+		Password: "12345678",
+	},
+	{
+		Name:     "  WsName  ", // Invalid name: Contains leading or trailing whitespace
+		Email:    "valid@test.com",
+		Password: "ValidPassword123",
+	},
+}
+
+func setupServer() (*sql.DB, *server.Server, *httptest.Server, *http.Client, error) {
+	db, err := sql.Open("sqlite3", "./test.db?_foreign_keys=true")
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	srv := server.Connect(db)
+	err = srv.DB.InitDatabase()
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	router := srv.Start()
+	testServer := httptest.NewServer(router)
+	cli := testServer.Client()
+	return db, srv, testServer, cli, nil
+}
+func signup(cli *http.Client, testServer *httptest.Server, user TestUser) (*http.Response, error) {
+	body, err := json.Marshal(user)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := cli.Post(testServer.URL+"/api/signup", "application/json", bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// err := createPost(cli, testServer, cookie, title, content, []string{"facts"})
+
+func createPost(cli *http.Client, sURL string, ck *http.Cookie, p TestPost) (*http.Response, error) {
+	requestBodyBytes, err := json.Marshal(p)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodPost, sURL+"/api/posts/create/", bytes.NewReader(requestBodyBytes))
+	if err != nil {
+		return nil, err
+	}
+	req.AddCookie(ck)
+	return cli.Do(req)
+}
+
 // TestWithAuth tests all routes that require authentication
 func TestWithAuth(t *testing.T) {
-	cli := testServer.Client()
-
+	db, _, testServer, cli, err := setupServer()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+	defer testServer.Close()
 	testUser := struct {
 		Name      string `json:"name"`
 		Email     string `json:"email"`
@@ -37,96 +172,137 @@ func TestWithAuth(t *testing.T) {
 	}
 
 	t.Run("signup", func(t *testing.T) {
-		body, err := json.Marshal(testUser)
+		resp, err := signup(cli, testServer, testUser)
 		if err != nil {
 			t.Fatal(err)
 		}
-
-		resp, err := cli.Post(testServer.URL+"/api/signup", "application/json", bytes.NewReader(body))
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// bug found, if the user is already present, we give back 400 instead of 409
 		if resp.StatusCode != http.StatusOK {
 			errBody, _ := io.ReadAll(resp.Body)
 			fmt.Println(string(errBody))
 			t.Fatalf("expected %d, got %d", http.StatusOK, resp.StatusCode)
 		}
 	})
-
-	var cookie *http.Cookie
-	t.Run("login", func(t *testing.T) {
-		body := fmt.Sprintf(`{ "login": "%v", "password": "%v" }`, testUser.Email, testUser.Password)
-		resp, err := cli.Post(testServer.URL+"/api/login", "application/json",
-			strings.NewReader(body))
+	t.Run("invalidSignup", func(t *testing.T) {
+		// signup with an invalid user (empry string)
+		invalidResp, err := signup(cli, testServer, TestUser{})
 		if err != nil {
 			t.Fatal(err)
 		}
-		if len(resp.Cookies()) == 0 {
-			t.Fatal("no cookies after login")
+		if invalidResp.StatusCode != http.StatusBadRequest {
+			t.Fatalf("expected %d, got %d", 400, invalidResp.StatusCode)
 		}
-		cookie = resp.Cookies()[0]
-
-		if resp.StatusCode != http.StatusOK {
-			t.Fatalf("expected %d, got %d", http.StatusOK, resp.StatusCode)
+		// Loop through all the invalid users to test all the nonDB errors.
+		for _, user := range invalidUsers {
+			invalidResp, err := signup(cli, testServer, user)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if invalidResp.StatusCode != http.StatusBadRequest {
+				t.Fatalf("expected %d, got %d", http.StatusBadRequest, invalidResp.StatusCode)
+			}
 		}
 	})
+	t.Run("login", func(t *testing.T) {
+		cookie = dummyLogin(t, cli, testServer, testUser)
+	})
 
-	// test create post
-	t.Run("createPost", func(t *testing.T) {
-		body := struct {
-			Title      string   `json:"title"`
-			Content    string   `json:"content"`
-			Categories []string `json:"categories"`
-		}{
-			Title:      "Test Title",
-			Content:    "Test Content",
-			Categories: []string{"facts"},
-		}
-		requestBodyBytes, err := json.Marshal(body)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		req, err := http.NewRequest(http.MethodPost, testServer.URL+"/api/posts/create/",
-			bytes.NewReader(requestBodyBytes))
+	// create a test for a post not found (404) error
+	t.Run("postNotFound", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, testServer.URL+"/api/posts/1234", nil)
 		if err != nil {
 			t.Fatal(err)
 		}
 		req.AddCookie(cookie)
-
 		resp, err := cli.Do(req)
 		if err != nil {
 			t.Fatal(err)
 		}
-		if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode != http.StatusNotFound {
+			t.Fatalf("expected %d, got %d", 404, resp.StatusCode)
+		}
+	})
+
+	// test create 5 posts
+	t.Run("createPost", func(t *testing.T) {
+		for i := 1; i <= 5; i++ {
+			p := TestPost{
+				Title:      fmt.Sprintf("Test Title %d", i),
+				Content:    fmt.Sprintf("Test Content %d", i),
+				Categories: []string{"facts"}, // Categories
+			}
+			resp, err := createPost(cli, testServer.URL, cookie, p)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if resp.StatusCode != http.StatusOK {
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					log.Println(err)
+				}
+				errorMessage := string(body)
+				t.Fatalf("expected %d, got %d, ErrBody: %v", 200, resp.StatusCode, errorMessage)
+			}
+		}
+	})
+
+	t.Run("createInvalidPost", func(t *testing.T) {
+		resp, err := createPost(cli, testServer.URL, cookie, TestPost{"", "", []string{""}})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if resp.StatusCode != http.StatusBadRequest {
 			body, err := io.ReadAll(resp.Body)
 			if err != nil {
 				log.Println(err)
 			}
 			errorMessage := string(body)
-			t.Fatalf("expected %d, got %d, ErrBody: %v", http.StatusOK, resp.StatusCode, errorMessage)
+			t.Fatalf("expected %d, got %d, ErrBody: %v", 400, resp.StatusCode, errorMessage)
+		}
+		// Tests the invalid posts
+		for _, post := range invalidPosts {
+			resp, err := createPost(cli, testServer.URL, cookie, post)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if resp.StatusCode != http.StatusBadRequest {
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					log.Println(err)
+				}
+				errorMessage := string(body)
+				t.Fatalf("expected %d, got %d, ErrBody: %v", 400, resp.StatusCode, errorMessage)
+			}
 		}
 	})
-
 	validTests := []struct {
+		name             string
 		url              string
 		body             []byte
 		expectedResponse string
 	}{
-		{"/api/posts/1/like", nil, "1"},
-		{"/api/posts/1/like", nil, "0"},
+		{"/api/posts/1/like return 1", "/api/posts/1/like", nil, "1"},
+		{"/api/posts/1/like return 0", "/api/posts/1/like", nil, "0"},
 
-		{"/api/posts/1/dislike", nil, "-1"},
-		{"/api/posts/1/dislike", nil, "0"},
-
-		{"/api/posts/1/comment", []byte(`{"content": "test"}`), "1"},
-		{"/api/posts/1/comment/1/like", nil, "1"},
-		{"/api/posts/1/comment/1/like", nil, "0"},
-		{"/api/posts/1/comment/1/dislike", nil, "-1"},
-		{"/api/posts/1/comment/1/dislike", nil, "0"},
-		{"/api/logout", nil, ""},
+		// test getting the posts liked by the user
+		{"/api/me/posts/liked return 0 POSTS", "/api/me/posts/liked", nil, "[]"},
+		// test disliking post 1 and then undisliking it by clicking again on the dislike button
+		{"/api/posts/1/dislike return -1", "/api/posts/1/dislike", nil, "-1"},
+		{"/api/posts/1/dislike return 0", "/api/posts/1/dislike", nil, "0"},
+		// dislike a post then like it, so that it returns like count as 1
+		{"/api/posts/3/dislike return -1", "/api/posts/3/dislike", nil, "-1"},
+		{"/api/posts/3/like return 1", "/api/posts/3/like", nil, "1"},
+		// like a post then like it, so that it returns like count as 1
+		{"/api/posts/3/dislike return -1", "/api/posts/3/dislike", nil, "-1"},
+		{"/api/posts/1/comment return 1", "/api/posts/1/comment", []byte(`{"content": "test"}`), "1"},
+		{"/api/posts/1/comment/1/like return 1", "/api/posts/1/comment/1/like", nil, "1"},
+		{"/api/posts/1/comment/1/like return 0", "/api/posts/1/comment/1/like", nil, "0"},
+		{"/api/posts/1/comment/1/dislike return -1", "/api/posts/1/comment/1/dislike", nil, "-1"},
+		{"/api/posts/1/comment/1/dislike return 0", "/api/posts/1/comment/1/dislike", nil, "0"},
+		// dislike a comment then like it, so that it returns like count as 1
+		{"/api/posts/1/comment/1/dislike return -1", "/api/posts/1/comment/1/dislike", nil, "-1"},
+		{"/api/posts/1/comment/1/like return 1", "/api/posts/1/comment/1/like", nil, "1"},
+		{"/api/posts/1/comment/1/dislike return -1", "/api/posts/1/comment/1/dislike", nil, "-1"},
+		{"/api/logout return empty string", "/api/logout", nil, ""},
 	}
 
 	for _, test := range validTests {
@@ -136,7 +312,6 @@ func TestWithAuth(t *testing.T) {
 				t.Fatal(err)
 			}
 			req.AddCookie(cookie)
-
 			resp, err := cli.Do(req)
 			if err != nil {
 				t.Fatal(err)
@@ -144,7 +319,6 @@ func TestWithAuth(t *testing.T) {
 			if resp.StatusCode != http.StatusOK {
 				t.Fatalf("expected %d, got %d", http.StatusOK, resp.StatusCode)
 			}
-
 			respBody, err := io.ReadAll(resp.Body)
 			if err != nil {
 				t.Fatal(err)
@@ -158,24 +332,20 @@ func TestWithAuth(t *testing.T) {
 
 // BenchmarkWithAuth benchmarks all routes that require authentication
 func BenchmarkWithAuth(b *testing.B) {
-
 	for i := 0; i < b.N; i++ {
 		cli := testServer.Client()
 		rand, err := uuid.NewV4()
 		if err != nil {
 			b.Error(err)
 		}
-
 		name := rand.String()[:15]
 		email := fmt.Sprintf("%s@test.com", name)
 		body := fmt.Sprintf(`{ "name": "%s", "email": "%s", "password": "notapassword" }`, name, email)
-
 		resp, err := cli.Post(testServer.URL+"/api/signup", "application/json",
 			strings.NewReader(body))
 		if err != nil {
 			b.Error(err)
 		}
-
 		if resp.StatusCode != http.StatusOK {
 			body, err := io.ReadAll(resp.Body)
 			if err != nil {
@@ -183,7 +353,6 @@ func BenchmarkWithAuth(b *testing.B) {
 			}
 			b.Errorf("expected %d, got %d, %s", http.StatusOK, resp.StatusCode, body)
 		}
-
 		resp, err = cli.Post(testServer.URL+"/api/login", "application/json",
 			strings.NewReader(fmt.Sprintf(`{ "login": "%s", "password": "notapassword" }`, email)))
 		if err != nil {
@@ -196,13 +365,11 @@ func BenchmarkWithAuth(b *testing.B) {
 		if resp.StatusCode != http.StatusOK {
 			b.Errorf("expected %d, got %d", http.StatusOK, resp.StatusCode)
 		}
-
 		request, err := http.NewRequest(http.MethodPost, testServer.URL+"/api/logout", nil)
 		if err != nil {
 			b.Error(err)
 		}
 		request.AddCookie(cookie)
-
 		resp, err = cli.Do(request)
 		if err != nil {
 			b.Errorf("error while logging out: %v", err)
@@ -214,4 +381,21 @@ func BenchmarkWithAuth(b *testing.B) {
 			b.Errorf("cookie should be expired")
 		}
 	}
+}
+
+func dummyLogin(t *testing.T, cli *http.Client, testServer *httptest.Server, testUser TestUser) *http.Cookie {
+	body := fmt.Sprintf(`{ "login": "%v", "password": "%v" }`, testUser.Email, testUser.Password)
+	resp, err := cli.Post(testServer.URL+"/api/login", "application/json",
+		strings.NewReader(body))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(resp.Cookies()) == 0 {
+		t.Fatal("no cookies after login")
+	}
+	cookie := resp.Cookies()[0]
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected %d, got %d", 200, resp.StatusCode)
+	}
+	return cookie
 }
