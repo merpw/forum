67ab27a7acb7d3d55638dbf4b9dc390adcc9bae6
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-06-01 17:34:17 +0300

    add main DaisyUI colors preset

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     container: {
       screens: {
-        xl: "800px",
+        xl: "1200px",
         // TODO: remove when we will have more content to display (e.g. sidebar)
       },
     },
@@ -24,6 +24,36 @@ module.exports = {
         },
       },
     },
+  },
+  daisyui: {
+    styled: true,
+    base: false,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark_theme",
+    themes: [
+      {
+        // TODO: choose colors
+        light_theme: {
+          "base-100": "#e0e0ea", // text
+          primary: "#6176cb", // username
+          secondary: "#ffac8d", // user's reaction
+          accent: "#69f19f", // online status
+          neutral: "#bebbc7", // break lines, background etc.
+          info: "#757575", // reactions, comments button etc.
+        },
+        dark_theme: {
+          "base-100": "#2c2c2c",
+          primary: "#505bdc",
+          secondary: "#ffa874",
+          accent: "#00e16a",
+          neutral: "#444444",
+          info: "#797979",
+        },
+      },
+    ],
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
