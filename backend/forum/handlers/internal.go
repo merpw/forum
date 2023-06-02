6d801490ad170b8c49d449f878445ef78f90463f
Maksim Mikhailov <max@mer.pw>
2023-06-02 20:56:35 +0300

    Add revoked-sessions SSE endpoint
    To instantly disconnect users, whose token has expired

--- backend/forum/handlers/internal.go
+++ backend/forum/handlers/internal.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"backend/common/server"
+	"log"
 	"net/http"
 )
 
@@ -24,4 +25,47 @@ func (h *Handlers) checkSession(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	server.SendObject(w, userId)
+}
+
+// revokedSessions returns an SSE stream of revoked sessions
+func (h *Handlers) revokedSessions(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "SSE not supported", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	flusher.Flush()
+
+	subscriber := make(chan string)
+
+	h.lock.Lock()
+	h.revokeSessionSubscribers = append(h.revokeSessionSubscribers, &subscriber)
+	h.lock.Unlock()
+
+loop:
+	for {
+		select {
+		case token := <-subscriber:
+			_, err := w.Write([]byte(token + "\n"))
+			if err != nil {
+				log.Println(err)
+				break loop
+			}
+			flusher.Flush()
+		case <-r.Context().Done():
+			break loop
+		}
+	}
+
+	for i, s := range h.revokeSessionSubscribers {
+		if s == &subscriber {
+			h.lock.Lock()
+			h.revokeSessionSubscribers = append(h.revokeSessionSubscribers[:i], h.revokeSessionSubscribers[i+1:]...)
+			h.lock.Unlock()
+			close(subscriber)
+			break
+		}
+	}
 }
