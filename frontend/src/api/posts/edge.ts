bfedacdeb09d7eceee88d2b1ed890d2d49423087
Maksim Mikhailov <max@mer.pw>
2023-06-03 22:27:43 +0300

    Add `Internal-Auth` header to all frontend server-side requests
    Improve next-public/ai error handling

--- frontend/src/api/posts/edge.ts
+++ frontend/src/api/posts/edge.ts
@@ -1,37 +1,61 @@
 import { Comment, Post } from "@/custom"
 
 export const getPostsLocal = (): Promise<Post[]> =>
-  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts`).then((res) => {
+  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts`, {
+    headers: {
+      "Internal-Auth": process.env.FORUM_BACKEND_SECRET || "secret",
+    },
+  }).then((res) => {
     if (!res.ok) throw new Error("Network response was not ok")
     return res.json()
   })
 
 export const getPostLocal = (id: number): Promise<Post> =>
-  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts/${id}`).then((res) => {
+  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts/${id}`, {
+    headers: {
+      "Internal-Auth": process.env.FORUM_BACKEND_SECRET || "secret",
+    },
+  }).then((res) => {
     if (!res.ok) throw new Error("Network response was not ok")
     return res.json()
   })
 
 export const getPostCommentsLocal = (id: number): Promise<Comment[]> =>
-  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts/${id}/comments`).then((res) => {
+  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts/${id}/comments`, {
+    headers: {
+      "Internal-Auth": process.env.FORUM_BACKEND_SECRET || "secret",
+    },
+  }).then((res) => {
     if (!res.ok) throw new Error("Network response was not ok")
     return res.json()
   })
 
 export const getCategoriesLocal = (): Promise<string[]> =>
-  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts/categories`).then((res) => {
+  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts/categories`, {
+    headers: {
+      "Internal-Auth": process.env.FORUM_BACKEND_SECRET || "secret",
+    },
+  }).then((res) => {
     if (!res.ok) throw new Error("Network response was not ok")
     return res.json()
   })
 
 export const getUserPostsLocal = (user_id: number): Promise<Post[]> =>
-  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/users/${user_id}/posts`).then((res) => {
+  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/users/${user_id}/posts`, {
+    headers: {
+      "Internal-Auth": process.env.FORUM_BACKEND_SECRET || "secret",
+    },
+  }).then((res) => {
     if (!res.ok) throw new Error("Network response was not ok")
     return res.json()
   })
 
 export const getCategoryPostsLocal = (category: string): Promise<Post[]> =>
-  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts/categories/${category}`).then((res) => {
+  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/posts/categories/${category}`, {
+    headers: {
+      "Internal-Auth": process.env.FORUM_BACKEND_SECRET || "secret",
+    },
+  }).then((res) => {
     if (!res.ok) throw new Error("Network response was not ok")
     return res.json()
   })
--- frontend/src/api/users/edge.ts
+++ frontend/src/api/users/edge.ts
@@ -1,7 +1,11 @@
 import { User } from "@/custom"
 
 export const getUserLocal = (id: number): Promise<User> =>
-  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/users/${id}`).then((res) => {
+  fetch(`${process.env.FORUM_BACKEND_PRIVATE_URL}/api/users/${id}`, {
+    headers: {
+      "Internal-Auth": process.env.FORUM_BACKEND_SECRET || "secret",
+    },
+  }).then((res) => {
     if (!res.ok) throw new Error("Network response was not ok")
     return res.json()
   })
