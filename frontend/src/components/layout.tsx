23c8e44d2d11720ff6f5dc07c8dd2beb6fc6500a
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-06-04 21:48:12 +0300

    add footer with Link to GitHub repository
    Prettier to Add footer

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,15 +1,31 @@
 import { ReactNode } from "react"
+import Link from "next/link"
 
 import Navbar from "./navbar"
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className={"container mx-auto p-5 min-h-screen break-words"}>
+    <div className={"container m-auto p-5 min-h-screen break-words"}>
       <header>
         <Navbar />
       </header>
       <main>{children}</main>
-      <hr className={"my-7 opacity-50"} />
+      <footer>
+        <div
+          className={
+            "text-center text-sm font-thin text-neutral border-t border-base-100 pt-3 mt-3"
+          }
+        >
+          <p>
+            {"Made with "}
+            <Link href={"https://github.com/merpw/forum"} className={"gradient-text clickable"}>
+              {"❤"}
+            </Link>
+            {" by "}
+            <span className={"font-light"}>{"basket of kittens"}</span>
+          </p>
+        </div>
+      </footer>
     </div>
   )
 }
