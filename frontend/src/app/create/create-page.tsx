45c9d7509cbba7bcf9acc7a5fcbf417ce24ed59f
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-06-04 22:38:53 +0300

    apply DaisyUI components
    card and text input/area components

--- frontend/src/app/create/create-page.tsx
+++ frontend/src/app/create/create-page.tsx
@@ -96,149 +96,166 @@ const CreatePostForm: FC<{ categories: string[]; isAIEnabled: boolean }> = ({
           .catch((err) => setFormError(err.message))
       }}
     >
-      <div className={"mb-3"}>
-        <input
-          type={"text"}
-          name={"title"}
-          className={"inputbox-singlerow sm:w-60"}
-          placeholder={"Title"}
-          value={formFields.title}
-          onChange={() => void 0 /* handled by Form */}
-          required
-          maxLength={25}
-        />
-      </div>
-      <div className={"mb-3"}>
-        <ReactTextAreaAutosize
-          title={"Content input field. Press ESC and then TAB to move the focus to the next field"}
-          onFocus={(e) => e.currentTarget.setAttribute("data-escaped", "false")}
-          onKeyDown={(e) => {
-            if (e.key === "Escape") {
-              return e.currentTarget.setAttribute("data-escaped", "true")
-            }
-
-            if (
-              e.currentTarget.getAttribute("data-escaped") === "false" &&
-              !e.shiftKey &&
-              e.key === "Tab"
-            ) {
-              e.preventDefault()
-              const textarea = e.target as HTMLTextAreaElement
-              const start = textarea.selectionStart
-              const end = textarea.selectionEnd
-              const value = textarea.value
-              textarea.value = value.substring(0, start) + "\t" + value.substring(end)
-              textarea.selectionStart = textarea.selectionEnd = start + 1
-              setFormFields({ ...formFields, content: textarea.value })
-            }
-          }}
-          name={"content"}
-          className={"inputbox"}
-          rows={5}
-          minRows={5}
-          placeholder={"Content"}
-          value={formFields.content}
-          required
-          maxLength={10000}
-        />
-      </div>
-
-      <div className={"mb-3"} id={"preview"}>
-        <Markdown content={formFields.content} />
-      </div>
-
-      <ReactTextAreaAutosize
-        name={"description"}
-        className={"inputbox mb-3"}
-        rows={2}
-        minRows={2}
-        placeholder={"Description"}
-        value={formFields.description}
-        maxLength={205}
-      />
-      {isAIEnabled && (
-        <button
-          onClick={async () => {
-            if (!formFields.title) {
-              return setFormError("Title is required")
-            }
-            if (!formFields.content) {
-              return setFormError("Content is required")
-            }
-            setFormFields({ ...formFields, isDescriptionLoading: true })
-            generateDescription(formFields)
-              .then((description) => {
-                setFormError(null)
-                setFormFields((prev) => ({ ...prev, description }))
-              })
-              .catch((err) => setFormError(err.message))
-              .finally(() => setFormFields((prev) => ({ ...prev, isDescriptionLoading: false })))
-          }}
-          type={"button"}
-          className={"button flex mb-3 p-2"}
-        >
-          {formFields.isDescriptionLoading ? (
-            <svg
-              aria-hidden={"true"}
-              className={"w-5 h-5 mr-1 text-gray-200 animate-spin fill-blue-600"}
-              viewBox={"0 0 100 101"}
-              fill={"none"}
-              xmlns={"http://www.w3.org/2000/svg"}
-            >
-              <path
-                d={
-                  "M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                }
-                fill={"currentColor"}
+      <div className={"max-w-3xl mx-auto flex-col"}>
+        <div className={"card flex-shrink-0 bg-neutral shadow-md"}>
+          <div className={"card-body"}>
+            <div className={"my-2 font-Yesteryear text-3xl text-primary opacity-50 text-center"}>
+              <span className={"text-base-100"}> {"What's on your mind?"}</span>
+            </div>
+            <div className={"form-control"}>
+              <input
+                type={"text"}
+                name={"title"}
+                className={"input input-bordered bg-base-100 text-sm"}
+                placeholder={"Title"}
+                value={formFields.title}
+                onChange={() => void 0 /* handled by Form */}
+                required
+                maxLength={25}
               />
-              <path
-                d={
-                  "M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            </div>
+            <div className={"form-control"}>
+              <ReactTextAreaAutosize
+                title={
+                  "Content input field. Press ESC and then TAB to move the focus to the next field"
                 }
-                fill={"currentFill"}
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns={"http://www.w3.org/2000/svg"}
-              fill={"none"}
-              viewBox={"0 0 24 24"}
-              strokeWidth={1.5}
-              stroke={"currentColor"}
-              className={"w-5 h-5 mr-1"}
-            >
-              <path
-                strokeLinecap={"round"}
-                strokeLinejoin={"round"}
-                d={
-                  "M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-                }
-              />
-            </svg>
-          )}
-          Generate description
-        </button>
-      )}
-      <div className={"mb-3"}>
-        <Select
-          placeholder={"Categories"}
-          instanceId={useId()}
-          isMulti={true}
-          name={"categories"}
-          className={"react-select-container"}
-          classNamePrefix={"react-select"}
-          onChange={(newValue) =>
-            setFormFields({ ...formFields, categories: newValue.map((v) => v.value) })
-          }
-          options={categories.map((name) => ({ label: Capitalize(name), value: name }))}
-          required
-        />
-      </div>
-      <FormError error={formError} />
+                onFocus={(e) => e.currentTarget.setAttribute("data-escaped", "false")}
+                onKeyDown={(e) => {
+                  if (e.key === "Escape") {
+                    return e.currentTarget.setAttribute("data-escaped", "true")
+                  }
 
-      <button type={"submit"} className={"button"}>
-        Submit
-      </button>
+                  if (
+                    e.currentTarget.getAttribute("data-escaped") === "false" &&
+                    !e.shiftKey &&
+                    e.key === "Tab"
+                  ) {
+                    e.preventDefault()
+                    const textarea = e.target as HTMLTextAreaElement
+                    const start = textarea.selectionStart
+                    const end = textarea.selectionEnd
+                    const value = textarea.value
+                    textarea.value = value.substring(0, start) + "\t" + value.substring(end)
+                    textarea.selectionStart = textarea.selectionEnd = start + 1
+                    setFormFields({ ...formFields, content: textarea.value })
+                  }
+                }}
+                name={"content"}
+                className={"textarea textarea-bordered"}
+                rows={5}
+                minRows={5}
+                placeholder={"Content"}
+                value={formFields.content}
+                required
+                maxLength={10000}
+              />
+            </div>
+
+            <div className={"mb-3 border border-base-100 rounded-xl mt-3 px-4"} id={"preview"}>
+              <Markdown content={formFields.content} />
+            </div>
+            <ReactTextAreaAutosize
+              name={"description"}
+              className={"textarea textarea-bordered"}
+              rows={2}
+              minRows={2}
+              placeholder={"Description"}
+              value={formFields.description}
+              maxLength={205}
+            />
+            {isAIEnabled && (
+              <button
+                onClick={async () => {
+                  if (!formFields.title) {
+                    return setFormError("Title is required")
+                  }
+                  if (!formFields.content) {
+                    return setFormError("Content is required")
+                  }
+                  setFormFields({ ...formFields, isDescriptionLoading: true })
+                  generateDescription(formFields)
+                    .then((description) => {
+                      setFormError(null)
+                      setFormFields((prev) => ({ ...prev, description }))
+                    })
+                    .catch((err) => setFormError(err.message))
+                    .finally(() =>
+                      setFormFields((prev) => ({ ...prev, isDescriptionLoading: false }))
+                    )
+                }}
+                type={"button"}
+                className={
+                  "btn btn-xs bg-gray-500 flex text-xs text-secondary font-light mb-3 self-center"
+                }
+              >
+                {formFields.isDescriptionLoading ? (
+                  <svg
+                    aria-hidden={"true"}
+                    className={"w-5 h-5 mr-1 text-secondary animate-spin fill-base-300"}
+                    viewBox={"0 0 100 101"}
+                    fill={"none"}
+                    xmlns={"http://www.w3.org/2000/svg"}
+                  >
+                    <path
+                      d={
+                        "M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      }
+                      fill={"currentColor"}
+                    />
+                    <path
+                      d={
+                        "M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      }
+                      fill={"currentFill"}
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns={"http://www.w3.org/2000/svg"}
+                    fill={"none"}
+                    viewBox={"0 0 24 24"}
+                    strokeWidth={1.5}
+                    stroke={"currentColor"}
+                    className={"w-5 h-5 mr-1 text-secondary"}
+                  >
+                    <path
+                      strokeLinecap={"round"}
+                      strokeLinejoin={"round"}
+                      d={
+                        "M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                      }
+                    />
+                  </svg>
+                )}
+                Generate description
+              </button>
+            )}
+
+            <div className={"mb-3"}>
+              <Select
+                placeholder={"Categories"}
+                instanceId={useId()}
+                isMulti={true}
+                name={"categories"}
+                className={"react-select-container"}
+                classNamePrefix={"react-select"}
+                onChange={(newValue) =>
+                  setFormFields({ ...formFields, categories: newValue.map((v) => v.value) })
+                }
+                options={categories.map((name) => ({ label: Capitalize(name), value: name }))}
+                required
+              />
+            </div>
+
+            <FormError error={formError} />
+            <div className={"form-control"}>
+              <button type={"submit"} className={"button self-center"}>
+                SUBMIT
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </form>
   )
 }
