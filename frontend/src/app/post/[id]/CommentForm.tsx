3c9b0026c1dec57e577014a9b5e76c672afcfe6a
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-06-04 22:49:54 +0300

    move Comment form on-top of comment section, apply DaisyUI components

--- frontend/src/app/post/[id]/CommentForm.tsx
+++ frontend/src/app/post/[id]/CommentForm.tsx
@@ -21,54 +21,55 @@ const CommentForm: FC<{ postId: number }> = ({ postId }) => {
   if (!isLoggedIn && !isLoading) return null
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
+    <div className={"form-control"}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
 
-        if (isSame) return
+          if (isSame) return
 
-        setIsSame(true)
-        if (formError != null) setFormError(null)
+          setIsSame(true)
+          if (formError != null) setFormError(null)
 
-        CreateComment(postId, text)
-          .then(() => {
-            setText("")
-            mutateComments()
-          })
-          .catch((err) => {
-            if (err.code == "ERR_BAD_REQUEST") {
-              setFormError(err.response?.data as string)
-            } else {
-              // TODO: unexpected error
-            }
-          })
-      }}
-      className={"mb-5"}
-    >
-      <div className={"mb-3"}>
-        <label
-          htmlFor={"comment-text"}
-          className={"block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
-        >
-          Write a comment:
-        </label>
+          CreateComment(postId, text)
+            .then(() => {
+              setText("")
+              mutateComments()
+            })
+            .catch((err) => {
+              if (err.code == "ERR_BAD_REQUEST") {
+                setFormError(err.response?.data as string)
+              } else {
+                // TODO: unexpected error
+              }
+            })
+        }}
+        className={"mb-5"}
+      >
+        <div className={"mb-3"}>
+          <label htmlFor={"comment-text"} className={"block mb-2 text-sm font-light"}>
+            Write a comment:
+          </label>
 
-        <ReactTextareaAutosize
-          id={"comment-text"}
-          className={"inputbox"}
-          value={text}
-          onInput={(e) => setText(e.currentTarget.value)}
-          required
-          rows={1}
-        />
-      </div>
+          <ReactTextareaAutosize
+            id={"comment-text"}
+            className={"textarea textarea-bordered w-full"}
+            value={text}
+            onInput={(e) => setText(e.currentTarget.value)}
+            required
+            rows={1}
+          />
+        </div>
 
-      <FormError error={formError} />
+        <FormError error={formError} />
 
-      <button type={"submit"} className={"button"}>
-        Submit
-      </button>
-    </form>
+        <div className={"text-center"}>
+          <button type={"submit"} className={"btn button"}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
