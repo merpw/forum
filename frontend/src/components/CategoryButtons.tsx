6473a604dfe114fd936932b1ef94bdacbe618851
Maksim Mikhailov <max@mer.pw>
2023-06-07 17:42:36 +0300

    Add shared layout for `/` and `/category/[name]`
    To fix bug with unnecessary mounts/unmounts of the same component
    
    Implement the route group to share layout between different routes
    
    Move category buttons to the separate client component
    
    Co-authored-by: Natalia Kim <93912586+nattikim@users.noreply.github.com>

---
+++ frontend/src/components/CategoryButtons.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { FC } from "react"
+import Link from "next/link"
+import { useParams } from "next/navigation"
+
+const CategoryButtons: FC<{ categories: string[] }> = ({ categories }) => {
+  const { name } = useParams()
+
+  const activeCategory = name ?? "NONE"
+
+  return (
+    <div className={"mb-5"}>
+      <div className={"text-center space-x-1"}>
+        <div className={"text-sm font-thin mb-1 text-center text-info"}>Categories:</div>
+        <span className={"font-black gradient-text"}>{"•"}</span>
+        {categories.map((category, key) => (
+          <span key={key} className={""}>
+            <Link
+              href={`/category/${category}`}
+              className={
+                "btn btn-xs btn-neutral font-light" +
+                " " +
+                (activeCategory.toLowerCase() === category.toLowerCase()
+                  ? "btn-disabled bg-secondary font-normal text-black dark:text-white opacity-70"
+                  : "")
+              }
+            >
+              {category}
+            </Link>
+          </span>
+        ))}
+        <span className={"font-black gradient-text"}>{"•"}</span>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryButtons
