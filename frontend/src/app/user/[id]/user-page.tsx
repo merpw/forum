b74cc11352cb53c196e505bf6765864b99aaeb71
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-06-08 14:14:05 +0300

    change Icon for Let's chat button

--- frontend/src/app/user/[id]/user-page.tsx
+++ frontend/src/app/user/[id]/user-page.tsx
@@ -64,6 +64,7 @@ const UserPage: NextPage<{ user: User; posts: Post[] }> = ({ user, posts }) => {
       <div className={"text-center m-3"}>
         {/* TODO: add valid endpoint */}
         <Link href={"/chat"} className={"button"}>
+          <span className={"ml-1 text-xs"}>{"Let's chat!"}</span>
           <span className={"my-auto"}>
             <svg
               xmlns={"http://www.w3.org/2000/svg"}
@@ -77,12 +78,11 @@ const UserPage: NextPage<{ user: User; posts: Post[] }> = ({ user, posts }) => {
                 strokeLinecap={"round"}
                 strokeLinejoin={"round"}
                 d={
-                  "M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                  "M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
                 }
               />
             </svg>
           </span>
-          <span className={"ml-1 text-xs"}>{"Let's chat!"}</span>
         </Link>
       </div>
       <div className={"mt-5"}>
