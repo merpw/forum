725cf2bb627cf9722d823345fac5cce1724c7599
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-06-16 16:03:56 +0300

    comment out Chat button

--- frontend/src/app/user/[id]/user-page.tsx
+++ frontend/src/app/user/[id]/user-page.tsx
@@ -3,7 +3,6 @@
 import { NextPage } from "next"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
-import Link from "next/link"
 
 import { Post, User } from "@/custom"
 import { useMe } from "@/api/auth"
@@ -59,30 +58,31 @@ const UserPage: NextPage<{ user: User; posts: Post[] }> = ({ user, posts }) => {
           </div>
         </div>
       </div>
+      {/* TODO: uncomment when chat is implemented, add 'import Link from "next/link"'
       <div className={"text-center m-3"}>
-        {/* TODO: add valid endpoint */}
-        <Link href={"/chat"} className={"button"}>
+      <Link href={"/chat"} className={"button"}>
           <span className={"my-auto"}>
             <svg
-              xmlns={"http://www.w3.org/2000/svg"}
-              fill={"none"}
-              viewBox={"0 0 24 24"}
-              strokeWidth={1.5}
-              stroke={"currentColor"}
-              className={"w-5 h-5 scale-x-[-1]"}
+                xmlns={"http://www.w3.org/2000/svg"}
+                fill={"none"}
+                viewBox={"0 0 24 24"}
+                strokeWidth={1.5}
+                stroke={"currentColor"}
+                className={"w-5 h-5 scale-x-[-1]"}
             >
               <path
-                strokeLinecap={"round"}
-                strokeLinejoin={"round"}
-                d={
-                  "M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
-                }
+                  strokeLinecap={"round"}
+                  strokeLinejoin={"round"}
+                  d={
+                    "M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
+                  }
               />
             </svg>
           </span>
-          <span className={"text-xs"}>{"Let's chat!"}</span>
-        </Link>
-      </div>
+        <span className={"text-xs"}>{"Let's chat!"}</span>
+      </Link>
+    </div>
+      */}
       <div className={"mt-5"}>
         <div className={"text-center"}>
           <h2 className={"tab tab-bordered tab-active cursor-default self-center mb-3"}>
