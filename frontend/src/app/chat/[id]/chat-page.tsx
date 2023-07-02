8f8fae1e5d2716bf4b4fa13fd177a86c76e62208
Maksim Mikhailov <max@mer.pw>
2023-07-02 23:56:32 +0300

    Move `/chat/bad_string` redirect to backend

--- frontend/src/app/chat/[id]/chat-page.tsx
+++ frontend/src/app/chat/[id]/chat-page.tsx
@@ -1,49 +1,32 @@
 "use client"
 
 import { FC, useEffect } from "react"
-import { useParams, useRouter } from "next/navigation"
+import { useRouter } from "next/navigation"
 import { useDispatch } from "react-redux"
 
-import { chatActions } from "@/store/chats"
 import { useChat } from "@/api/chats/chats"
 import ChatInfo from "@/components/chats/ChatInfo"
 import ChatMessages from "@/components/chats/Messages"
 import WriteMessageForm from "@/components/chats/WriteMessageForm"
+import { chatActions } from "@/store/chats"
 
-const ChatPage = () => {
-  const chatId = Number(useParams().id)
-
-  const router = useRouter()
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    if (isNaN(chatId) || chatId < 0) {
-      router.push("/chat")
-      return
-    }
-    dispatch(chatActions.setActiveChatId(chatId))
-
-    return () => {
-      dispatch(chatActions.setActiveChatId(null))
-    }
-  }, [chatId, dispatch, router])
-
-  if (isNaN(chatId) || chatId < 0) {
-    return null
-  }
-
-  return <Chat id={chatId} />
-}
-
-const Chat: FC<{ id: number }> = ({ id }) => {
+const ChatPage: FC<{ id: number }> = ({ id }) => {
   const { chat } = useChat(id)
+  const dispatch = useDispatch()
   const router = useRouter()
 
   useEffect(() => {
     if (chat === null) {
       router.push("/chat")
     }
-  }, [chat, router])
+    if (chat) {
+      dispatch(chatActions.setActiveChatId(chat.id))
+
+      return () => {
+        dispatch(chatActions.setActiveChatId(null))
+      }
+    }
+  }, [chat, dispatch, id, router])
 
   if (!chat) {
     return <div>loading...</div>
--- frontend/src/app/chat/[id]/page.tsx
+++ frontend/src/app/chat/[id]/page.tsx
@@ -1,7 +1,16 @@
+import { redirect } from "next/navigation"
+
 import ChatPage from "@/app/chat/[id]/chat-page"
 
-const Page = () => {
-  return <ChatPage />
+type Props = { params: { id: string } }
+
+const Page = ({ params }: Props) => {
+  const id = +params.id
+  if (isNaN(id) || id < 0) {
+    return redirect("/chat")
+  }
+
+  return <ChatPage id={id} />
 }
 
 export default Page
