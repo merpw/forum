941c49405bdd70ee676ab412347117e7c932b5ab
Maksim Mikhailov <max@mer.pw>
2023-07-18 17:28:28 +0300

    Update `production` configuration

--- production/backup.sh
+++ production/backup.sh
@@ -2,4 +2,6 @@
 
 BACKUP_DIR=${BACKUP_DIR:-$(pwd)}
 
-docker cp forum-backend:/app/db/database.db "$BACKUP_DIR/database-$(/usr/bin/date "+%Y-%m-%d_%H-%M").db"
+docker cp forum-backend-forum:/app/db/database.db "$BACKUP_DIR/database-$(/usr/bin/date "+%Y-%m-%d_%H-%M").db"
+
+docker cp forum-backend-chat:/app/db/chat.db "$BACKUP_DIR/chat-$(/usr/bin/date "+%Y-%m-%d_%H-%M").db"
