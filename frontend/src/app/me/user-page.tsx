9b58f60bbb959902cb9e27332127c7b5870b5f7d
Maksim Mikhailov <max@mer.pw>
2023-08-02 19:31:53 +0300

    Move `UserInfo` from `/me page` to `components/profiles`

--- frontend/src/app/me/user-page.tsx
+++ frontend/src/app/me/user-page.tsx
@@ -1,19 +1,15 @@
 "use client"
 
-/* eslint-disable import/no-named-as-default-member */
-/* https://github.com/iamkun/dayjs/issues/1242 */
-import dayjs from "dayjs"
 import Link from "next/link"
 import { NextPage } from "next/types"
-import { FC, useState } from "react"
-import relativeTime from "dayjs/plugin/relativeTime"
+import { useState } from "react"
 
 import { useMe } from "@/api/auth/hooks"
 import { useMyPosts } from "@/api/posts/my_posts"
 import { useMyPostsLiked } from "@/api/posts/my_posts_liked"
 import { PostList } from "@/components/posts/list"
-import { User } from "@/custom"
 import Avatar from "@/components/Avatar"
+import { UserInfo } from "@/components/profiles/UserInfo"
 
 /* TODO: add placeholders */
 
@@ -95,62 +91,6 @@ const UserPage: NextPage = () => {
           </ul>
         </div>
         <div>{tabs[activeTab].component}</div>
-      </div>
-    </>
-  )
-}
-
-/* "2000-01-24" -> "23 years"
- * "2021-01-24" -> "baby 👶"
- */
-const calculateAge = (dob: string): string | null => {
-  dayjs.extend(relativeTime)
-
-  const parsedDob = dayjs(dob, "YYYY-MM-DD")
-  if (!parsedDob.isValid()) return null
-
-  const age = parsedDob.fromNow(true)
-  return age.includes("year") ? age + " old" : "baby 👶"
-}
-
-const UserInfo: FC<{ user: User }> = ({ user }) => {
-  const age = user.dob ? calculateAge(user.dob) : null
-  return (
-    <>
-      <div
-        className={"flex flex-col self-center font-light mb-5 text-center sm:text-left text-info"}
-      >
-        {"Hey, "}
-        <span className={"text-3xl sm:text-4xl text-primary font-Yesteryear mx-1"}>
-          {user.username}
-        </span>
-        {"Forgot who you are?"}
-      </div>
-      <div className={"font-light"}>
-        {user.first_name || user.last_name ? (
-          <p>
-            <span className={"font-light text-info"}>{"Full name"}</span>
-            <span
-              className={"font-normal start-dot"}
-            >{`${user.first_name} ${user.last_name}`}</span>
-          </p>
-        ) : null}
-        {age ? (
-          <p>
-            <span className={"font-light text-info"}>{"Age"}</span>
-            <span className={"font-normal start-dot"}>{age}</span>
-          </p>
-        ) : null}
-        {user.gender ? (
-          <p>
-            <span className={"font-light text-info"}>{"Gender"}</span>
-            <span className={"font-normal start-dot"}>{user.gender}</span>
-          </p>
-        ) : null}
-        <p>
-          <span className={"font-light text-info"}>{"Email"}</span>
-          <span className={"font-normal start-dot"}>{user.email}</span>
-        </p>
       </div>
     </>
   )
