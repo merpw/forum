fea062c1e472e8cfd8210a0230644b4356e01c7a
Maksim Mikhailov <max@mer.pw>
2023-08-03 23:38:51 +0300

    Add `avatar` and `bio` to SafeUser

--- backend/forum/handlers/me.go
+++ backend/forum/handlers/me.go
@@ -18,17 +18,18 @@ func (h *Handlers) me(w http.ResponseWriter, r *http.Request) {
 		LastName  string `json:"last_name,omitempty"`
 		DoB       string `json:"dob,omitempty"`
 		Gender    string `json:"gender,omitempty"`
-		Avatar    string `json:"avatar,omitempty"`
-		Bio       string `json:"bio,omitempty"`
 	}{
-		SafeUser:  SafeUser{Id: user.Id, Username: user.Username},
+		SafeUser: SafeUser{
+			Id:       user.Id,
+			Username: user.Username,
+			Avatar:   user.Avatar.String,
+			Bio:      user.Bio.String,
+		},
 		Email:     user.Email,
 		FirstName: user.FirstName.String,
 		LastName:  user.LastName.String,
 		DoB:       user.DoB.String,
 		Gender:    user.Gender.String,
-		Avatar:    user.Avatar.String,
-		Bio:       user.Bio.String,
 	}
 
 	server.SendObject(w, response)
@@ -49,10 +50,15 @@ func (h *Handlers) mePosts(w http.ResponseWriter, r *http.Request) {
 	for _, post := range posts {
 		response = append(response, Response{
 			SafePost: SafePost{
-				Id:            post.Id,
-				Title:         post.Title,
-				Description:   post.Description,
-				Author:        SafeUser{Id: user.Id, Username: user.Username},
+				Id:          post.Id,
+				Title:       post.Title,
+				Description: post.Description,
+				Author: SafeUser{
+					Id:       user.Id,
+					Username: user.Username,
+					Avatar:   user.Avatar.String,
+					Bio:      user.Bio.String,
+				},
 				Date:          post.Date,
 				CommentsCount: post.CommentsCount,
 				LikesCount:    post.LikesCount,
@@ -81,10 +87,15 @@ func (h *Handlers) mePostsLiked(w http.ResponseWriter, r *http.Request) {
 		author := h.DB.GetUserById(post.AuthorId)
 		response = append(response, Response{
 			SafePost: SafePost{
-				Id:            post.Id,
-				Title:         post.Title,
-				Description:   post.Description,
-				Author:        SafeUser{Id: author.Id, Username: author.Username},
+				Id:          post.Id,
+				Title:       post.Title,
+				Description: post.Description,
+				Author: SafeUser{
+					Id:       author.Id,
+					Username: author.Username,
+					Avatar:   author.Avatar.String,
+					Bio:      author.Bio.String,
+				},
 				Date:          post.Date,
 				CommentsCount: post.CommentsCount,
 				LikesCount:    post.LikesCount,
