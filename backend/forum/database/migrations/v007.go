5267d9c75424600f3cc1ab7743e9a50de81f7e68
bomanviktor <boman.viktor@gmail.com>
2023-08-04 12:02:19 +0300

    Add 'followers' table to 'v007'.
    Drop 'followers' on down migrate.

--- backend/forum/database/migrations/v007.go
+++ backend/forum/database/migrations/v007.go
@@ -9,6 +9,14 @@ import (
 var v007 = migrate.Migration{
 	Up: func(db *sql.DB) error {
 		_, err := db.Exec(`
+		CREATE TABLE followers (
+		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+			user_id INTEGER NOT NULL,
+			follower_id INTEGER NOT NULL,
+			timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+		    FOREIGN KEY (user_id) REFERENCES users(id),
+			FOREIGN KEY (follower_id) REFERENCES users(id)              
+		);
 		ALTER TABLE users 
 		    ADD COLUMN privacy INTEGER DEFAULT 1;
 		UPDATE users
@@ -24,6 +32,7 @@ var v007 = migrate.Migration{
 
 		_, err := db.Exec(`
 		ALTER TABLE users DROP COLUMN privacy;
+		DROP TABLE followers;
 		VACUUM;
 `)
 		if err != nil {
