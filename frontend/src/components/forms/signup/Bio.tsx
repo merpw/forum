0ab0693ba203fdc05005f8e6e5ac996d61f2714d
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-08-05 18:20:48 +0300

    Features/93 design (#100)
    * replace cartoon cat avatars with hacker animal avatars
    
    * change username placeholder text in signup form back to Username
    
    change text color for "optional" tag to neutral

--- frontend/src/components/forms/signup/Bio.tsx
+++ frontend/src/components/forms/signup/Bio.tsx
@@ -5,7 +5,7 @@ const Bio: FC = () => {
     <div className={"form-control"}>
       <label className={"label"}>
         <span className={"label-text"}>Bio</span>
-        <span className={"label-text-alt"}>optional</span>
+        <span className={"label-text-alt text-neutral"}>optional</span>
       </label>
       <textarea
         name={"bio"}
