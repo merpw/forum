9755e88665fb9b821b592cd405fca9874c45d702
Maksim Mikhailov <max@mer.pw>
2023-08-07 20:28:10 +0300

    Improve ReactSelect styles
    Implement custom Select component with tailwind-powered styles
    
    Remove styles from global.css

---
+++ frontend/src/components/Select.tsx
@@ -0,0 +1,43 @@
+import ReactSelect from "react-select"
+
+const Select: ReactSelect = (props) => {
+  return (
+    <ReactSelect
+      {...props}
+      unstyled={true}
+      className={""}
+      styles={{
+        control: (provided) => ({
+          ...provided,
+          transition: undefined,
+          outline: undefined,
+        }),
+      }}
+      classNames={{
+        control: ({ isFocused }) =>
+          "input pr-0" +
+          " " +
+          (isFocused ? "outline outline-offset-2 outline-base-content/20" : "text-gray-400"),
+
+        placeholder: () => "text-sm text-gray-400",
+
+        menu: () => "bg-base-100 my-1 mt-1 rounded",
+        option: ({ isFocused }) => "p-2" + " " + (isFocused ? "bg-base-200" : "bg-base-100"),
+        noOptionsMessage: () => "my-3",
+
+        indicatorsContainer: () => "m-1",
+        indicatorSeparator: () => "my-2 bg-gray-400",
+
+        dropdownIndicator: () => "p-2",
+        clearIndicator: () => "p-2",
+
+        valueContainer: () => "flex gap-1",
+        multiValue: () => "bg-neutral text-base-content rounded-md",
+        multiValueLabel: () => "m-2 my-1",
+        multiValueRemove: () => "hover:bg-error px-2 rounded-md",
+      }}
+    />
+  )
+}
+
+export default Select
--- frontend/src/components/forms/create-post/SelectCategories.tsx
+++ frontend/src/components/forms/create-post/SelectCategories.tsx
@@ -1,20 +1,16 @@
 import { FC, useId } from "react"
-import Select from "react-select"
 
 import { Capitalize } from "@/helpers/text"
-
-// TODO: maybe replace with another select component
+import Select from "@/components/Select"
 
 const SelectCategories: FC<{ categories: string[] }> = ({ categories }) => {
   return (
     <div className={"mb-3"}>
       <Select
+        name={"categories"}
         placeholder={"Categories"}
         instanceId={useId()}
         isMulti={true}
-        name={"categories"}
-        className={"react-select-container"}
-        classNamePrefix={"react-select"}
         options={categories.map((name) => ({ label: Capitalize(name), value: name }))}
         required
       />
