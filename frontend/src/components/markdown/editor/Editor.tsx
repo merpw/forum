1676bc93333b1b4596e6c12a2205bd86bd63cc96
Maksim Mikhailov <max@mer.pw>
2023-08-09 03:04:57 +0300

    Move submit button into MarkdownEditor
    Add `withSubmit` option
    
    Fix ref in MarkdownEditor, use React.forwardRef to safely forward refs
    
    Remove unnecessary states, reduce re-renders on input

--- frontend/src/components/markdown/editor/Editor.tsx
+++ frontend/src/components/markdown/editor/Editor.tsx
@@ -1,4 +1,15 @@
-import { FC, lazy, RefObject, Suspense, useCallback, useEffect, useRef, useState } from "react"
+import {
+  forwardRef,
+  ForwardRefRenderFunction,
+  lazy,
+  RefObject,
+  Suspense,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react"
 import ReactTextAreaAutosize, { TextareaAutosizeProps } from "react-textarea-autosize"
 import { useDropzone } from "react-dropzone"
 
@@ -11,20 +22,27 @@ const MaxImageSize = 1024 * 1024 * 20 // 20 MB
 
 const Markdown = lazy(() => import("@/components/markdown/markdown"))
 
-const MarkdownEditor: FC<
+const MarkdownEditor: ForwardRefRenderFunction<
+  HTMLTextAreaElement,
   TextareaAutosizeProps & {
-    ref?: RefObject<HTMLTextAreaElement>
+    withSubmit?: boolean
   }
-> = (props) => {
+> = (props, forwardedRef) => {
   const [isPreview, setIsPreview] = useState(false)
 
   const fallbackRef = useRef<HTMLTextAreaElement>(null)
 
-  const ref = props.ref || fallbackRef
+  const ref = (forwardedRef || fallbackRef) as RefObject<HTMLTextAreaElement>
 
   const selectionBackup = useRef<{ selectionStart: number; selectionEnd: number } | null>(null)
 
   const [uploadError, setUploadError] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (ref.current?.value === "" && isPreview) {
+      setIsPreview(false)
+    }
+  }, [isPreview, ref, ref.current?.value])
 
   useEffect(() => {
     const textarea = ref.current as HTMLTextAreaElement
@@ -90,10 +108,18 @@ const MarkdownEditor: FC<
     noClick: true,
   })
 
+  const innerProps = useMemo(() => {
+    const propsCopy = { ...props }
+    delete propsCopy.withSubmit
+    return propsCopy
+  }, [props])
+
   return (
     <div
       className={
-        "bg-base-200 rounded" + " " + (isDragActive ? "ring ring-opacity-60 ring-primary" : "")
+        "relative bg-base-200 rounded" +
+        " " +
+        (isDragActive ? "ring ring-opacity-60 ring-primary" : "")
       }
       {...getRootProps()}
       tabIndex={-1}
@@ -108,30 +134,53 @@ const MarkdownEditor: FC<
         </span>
         <span
           className={"tab tab-lifted" + " " + (isPreview ? "tab-active" : "")}
-          onClick={() => setIsPreview(true)}
+          onClick={() => {
+            if (ref.current?.value !== "") {
+              setIsPreview(true)
+            }
+          }}
         >
           Preview
         </span>
         {!isPreview && <ToolBar textareaRef={ref} />}
       </div>
       <ReactTextAreaAutosize
-        contentEditable={true}
-        {...props}
+        {...innerProps}
         ref={ref}
+        value={props.value}
         onChange={(e) => {
           props.onChange?.(e)
           setUploadError(null)
         }}
-        className={isPreview ? "hidden" : props.className + " " + ""}
+        className={
+          isPreview ? "hidden" : props.className + " " + (props.withSubmit ? "py-3 px-3 pr-10" : "")
+        }
         onPaste={(e) => {
           if (e.clipboardData.files.length > 0) {
             e.preventDefault()
             uploadImage(e.clipboardData.files[0])
           }
         }}
+        onKeyDown={
+          props.withSubmit
+            ? (e) => {
+                if (e.key === "Enter" && !e.shiftKey) {
+                  e.preventDefault()
+
+                  // trim input
+                  ref.current?.blur()
+                  ref.current?.focus()
+
+                  ref.current?.form?.dispatchEvent(
+                    new Event("submit", { cancelable: true, bubbles: true })
+                  )
+                }
+              }
+            : undefined
+        }
       />
       {isPreview ? (
-        <div className={"min-h-16"}>
+        <div className={"min-h-[5.15rem]"}>
           <Suspense fallback={<div className={"prose bg-base-100 p-5 rounded-b"}>Loading...</div>}>
             <Markdown
               content={ref.current?.value || "Nothing to preview"}
@@ -159,8 +208,35 @@ const MarkdownEditor: FC<
           </button>
         </>
       )}
+      {props.withSubmit && (
+        <button
+          className={
+            "absolute z-10 clickable disabled:opacity-50 right-3 bottom-11" +
+            " " +
+            (ref.current?.value === "" ? "btn-disabled opacity-50" : "")
+          }
+          type={"submit"}
+        >
+          <svg
+            xmlns={"http://www.w3.org/2000/svg"}
+            fill={"none"}
+            viewBox={"0 0 24 24"}
+            strokeWidth={2}
+            stroke={"currentColor"}
+            className={"w-7 h-7 text-primary"}
+          >
+            <path
+              strokeLinecap={"round"}
+              strokeLinejoin={"round"}
+              d={
+                "M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+              }
+            />
+          </svg>
+        </button>
+      )}
     </div>
   )
 }
 
-export default MarkdownEditor
+export default forwardRef(MarkdownEditor)
--- frontend/src/components/posts/CommentForm.tsx
+++ frontend/src/components/posts/CommentForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { FC, useEffect, useRef, useState } from "react"
+import { FC, useRef, useState } from "react"
 
 import { useMe } from "@/api/auth/hooks"
 import { CreateComment, useComments } from "@/api/posts/comment"
@@ -11,12 +11,9 @@ const CommentForm: FC<{ postId: number }> = ({ postId }) => {
   const { isLoggedIn, isLoading } = useMe()
   const { mutate: mutateComments } = useComments(postId)
 
-  const [input, setInput] = useState("")
   const [formError, setFormError] = useState<string | null>(null)
 
   const [isSame, setIsSame] = useState(false)
-
-  useEffect(() => setIsSame(false), [input])
 
   const formRef = useRef<HTMLFormElement>(null)
 
@@ -25,6 +22,7 @@ const CommentForm: FC<{ postId: number }> = ({ postId }) => {
   return (
     <div className={"form-control"}>
       <form
+        onChange={() => setIsSame(false)}
         ref={formRef}
         onSubmit={(e) => {
           e.preventDefault()
@@ -34,9 +32,14 @@ const CommentForm: FC<{ postId: number }> = ({ postId }) => {
           setIsSame(true)
           if (formError != null) setFormError(null)
 
+          const commentTextarea = formRef.current?.querySelector(
+            "[name=comment-text]"
+          ) as HTMLTextAreaElement
+          const input = commentTextarea.value
+
           CreateComment(postId, input)
             .then(() => {
-              setInput("")
+              commentTextarea.value = ""
               mutateComments()
             })
             .catch((err) => {
@@ -54,52 +57,13 @@ const CommentForm: FC<{ postId: number }> = ({ postId }) => {
           </label>
 
           <MarkdownEditor
+            name={"comment-text"}
+            withSubmit
             id={"comment-text"}
-            className={"input input-bordered w-full py-3 px-3 pr-10"}
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault()
-                const trimmedInput = input.trim()
-                setInput(trimmedInput)
-                if (trimmedInput === "") {
-                  return
-                }
-                formRef.current?.dispatchEvent(
-                  new Event("submit", { cancelable: true, bubbles: true })
-                )
-              }
-            }}
+            className={"input input-bordered w-full"}
             required
             maxRows={10}
           />
-
-          <button
-            className={
-              "absolute z-10 clickable disabled:opacity-50 right-3 bottom-5" +
-              " " +
-              (input === "" ? " btn-disabled opacity-50" : "")
-            }
-            type={"submit"}
-          >
-            <svg
-              xmlns={"http://www.w3.org/2000/svg"}
-              fill={"none"}
-              viewBox={"0 0 24 24"}
-              strokeWidth={2}
-              stroke={"currentColor"}
-              className={"w-7 h-7 text-primary"}
-            >
-              <path
-                strokeLinecap={"round"}
-                strokeLinejoin={"round"}
-                d={
-                  "M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                }
-              />
-            </svg>
-          </button>
         </div>
 
         <FormError error={formError} />
