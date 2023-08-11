3b14e18298f88b9fd4a43fde3fcf5167ebf41c12
Maksim Mikhailov <max@mer.pw>
2023-08-11 19:32:10 +0300

    Refactor groups_create_test
    Improve to follow test independence principle

--- backend/forum/handlers/test/groups_create_test.go
+++ backend/forum/handlers/test/groups_create_test.go
@@ -12,71 +12,83 @@ import (
 func TestCreateGroup(t *testing.T) {
 	testServer := NewTestServer(t)
 
-	cli1 := testServer.TestClient()
-
 	invalidBody := CreateGroup("test title", "test desc", []int{1})
 
 	t.Run("Invalid", func(t *testing.T) {
+		cli := testServer.TestClient()
 
 		t.Run("Unauthorized", func(t *testing.T) {
-			cli1.TestPost(t, "/api/groups/create", invalidBody, http.StatusUnauthorized)
+			cli.TestPost(t, "/api/groups/create", invalidBody, http.StatusUnauthorized)
 		})
 
-		cli1.TestAuth(t)
+		cli.TestAuth(t)
 
 		t.Run("Method", func(t *testing.T) {
-			cli1.TestGet(t, "/api/groups/create", http.StatusMethodNotAllowed)
+			cli.TestGet(t, "/api/groups/create", http.StatusMethodNotAllowed)
 		})
 
 		t.Run("Invite yourself", func(t *testing.T) {
-			cli1.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
+			cli.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
 		})
 
 		t.Run("Invite invalid user", func(t *testing.T) {
 			invalidBody.Invite = []int{1337}
-			cli1.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
+			cli.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
 		})
 
 		t.Run("Invalid requestBody", func(t *testing.T) {
-			cli1.TestPost(t, "/api/groups/create", "test", http.StatusBadRequest)
+			cli.TestPost(t, "/api/groups/create", "test", http.StatusBadRequest)
 		})
 
 		t.Run("Too short title", func(t *testing.T) {
 			invalidBody.Title = ""
-			cli1.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
+			cli.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
 		})
 
 		invalidBody.Title = strings.Repeat("t", 201)
 		t.Run("Too long title", func(t *testing.T) {
-			cli1.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
+			cli.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
 		})
 
 		t.Run("Too short description", func(t *testing.T) {
 			invalidBody.Title = "Test"
 			invalidBody.Description = ""
-			cli1.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
+			cli.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
 		})
 		t.Run("Too long description", func(t *testing.T) {
 			invalidBody.Description = strings.Repeat("t", 201)
-			cli1.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
+			cli.TestPost(t, "/api/groups/create", invalidBody, http.StatusBadRequest)
 		})
 	})
 
 	var invites []int
 
-	for i := 2; i <= 6; i++ {
+	// Creates a group with valid title, description, and with 6 users in it
+	for i := 0; i <= 5; i++ {
 		client := testServer.TestClient()
+
 		client.TestAuth(t)
-		invites = append(invites, i)
+		var meResp struct {
+			Id int `json:"id"`
+		}
+		_, respBody := client.TestGet(t, "/api/me", http.StatusOK)
+		err := json.Unmarshal(respBody, &meResp)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		invites = append(invites, meResp.Id)
 	}
 
-	// Creates a group with valid title, description, and with 6 users in it
 	validBody := CreateGroup("test title", "test desc", invites)
 
 	t.Run("Valid", func(t *testing.T) {
-		t.Run("Create", func(t *testing.T) {
+		cli := testServer.TestClient()
+		cli.TestAuth(t)
+
+		t.Run("Full", func(t *testing.T) {
 			var groupId int
-			_, respBody := cli1.TestPost(t, "/api/groups/create", validBody, http.StatusOK)
+			_, respBody := cli.TestPost(t, "/api/groups/create", validBody, http.StatusOK)
 
 			err := json.Unmarshal(respBody, &groupId)
 			if err != nil {
@@ -86,41 +98,45 @@ func TestCreateGroup(t *testing.T) {
 			if groupId != 1 {
 				t.Errorf("unexpected group id, expected %d, got %d", 1, groupId)
 			}
-		})
 
-		var groups []int
-		t.Run("Get all groups", func(t *testing.T) {
-			_, resp := cli1.TestGet(t, "/api/groups", http.StatusOK)
-			if err := json.Unmarshal(resp, &groups); err != nil {
+			cli.TestGet(t, fmt.Sprintf("/api/groups/%d", groupId), http.StatusOK)
+
+			_, resp := cli.TestGet(t, "/api/groups", http.StatusOK)
+			var groupIds []int
+			if err := json.Unmarshal(resp, &groupIds); err != nil {
 				t.Fatal(err)
 			}
-
-			if len(groups) != 1 {
-				t.Errorf("unexpected amount of groups, expected %d, got %d", 1, len(groups))
+			// Check if the group is in the list of groups
+			found := false
+			for _, id := range groupIds {
+				if id == groupId {
+					found = true
+					break
+				}
+			}
+			if !found {
+				t.Errorf("group not found in list of groups")
 			}
 
-		})
+			endpoint := fmt.Sprintf("/api/groups/%d", groupId)
 
-		responseBody := struct {
-			Id           int    `json:"id"`
-			Title        string `json:"title"`
-			Description  string `json:"description"`
-			MemberStatus int    `json:"member_status"`
-			Members      int    `json:"member_count"`
-		}{}
+			_, respBody = cli.TestGet(t, endpoint, http.StatusOK)
 
-		t.Run("Get group info", func(t *testing.T) {
-			endpoint := fmt.Sprintf("/api/groups/%d", groups[0])
+			responseBody := struct {
+				Id           int    `json:"id"`
+				Title        string `json:"title"`
+				Description  string `json:"description"`
+				MemberStatus int    `json:"member_status"`
+				Members      int    `json:"member_count"`
+			}{}
 
-			_, respBody := cli1.TestGet(t, endpoint, http.StatusOK)
-
-			err := json.Unmarshal(respBody, &responseBody)
+			err = json.Unmarshal(respBody, &responseBody)
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			if responseBody.Id != groups[0] {
-				t.Errorf("invalid id, expected %d, got %d", groups[0], responseBody.Id)
+			if responseBody.Id != groupId {
+				t.Errorf("invalid id, expected %d, got %d", groupId, responseBody.Id)
 			}
 
 			if responseBody.Title != validBody.Title {
@@ -138,8 +154,13 @@ func TestCreateGroup(t *testing.T) {
 			if responseBody.Members != 1 {
 				t.Errorf("invalid members, expected %d, got %d", 1, responseBody.Members)
 			}
-
 		})
+	})
+	t.Run("No invites", func(t *testing.T) {
+		cli := testServer.TestClient()
+		cli.TestAuth(t)
 
+		validBody.Invite = []int{}
+		cli.TestPost(t, "/api/groups/create", validBody, http.StatusOK)
 	})
 }
