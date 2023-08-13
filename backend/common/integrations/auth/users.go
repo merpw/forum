c181041b46fc94517aefcd36440083c33561b530
Maksim Mikhailov <max@mer.pw>
2023-08-13 18:37:37 +0300

    Refactor `integrations/auth`
    Add InternalRequest() function to simplify internal request building

--- backend/common/integrations/auth/users.go
+++ backend/common/integrations/auth/users.go
@@ -1,10 +1,7 @@
 package auth
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
-	"os"
 	"strconv"
 )
 
@@ -15,30 +12,6 @@ type User struct {
 
 // GetUser gets user data from the auth service. Returns nil if user not found.
 func GetUser(userId int) (user *User) {
-	req, err := http.NewRequest(http.MethodGet, os.Getenv("AUTH_BASE_URL")+"/api/users/"+strconv.Itoa(userId), nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("Internal-Auth", os.Getenv("FORUM_BACKEND_SECRET"))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	if resp.StatusCode == http.StatusNotFound {
-		return nil
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		log.Panicf("status code: %d", resp.StatusCode)
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&user)
-	if err != nil {
-		panic(err)
-	}
-	_ = resp.Body.Close()
-
+	InternalRequest(http.MethodGet, "/api/users/"+strconv.Itoa(userId), nil, &user)
 	return user
 }
