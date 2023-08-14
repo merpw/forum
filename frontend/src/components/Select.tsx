4bd4cb3ade21c73d56131d8a3fff07c29fb1bf38
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-08-14 14:16:06 +0300

    fix Select component styles
    for selecting followers

--- frontend/src/components/Select.tsx
+++ frontend/src/components/Select.tsx
@@ -15,25 +15,25 @@ const Select: ReactSelect = (props) => {
       }}
       classNames={{
         control: ({ isFocused }) =>
-          "input pr-0" +
+          "relative input " +
           " " +
           (isFocused ? "outline outline-offset-2 outline-base-content/20" : "text-gray-400"),
 
         placeholder: () => "text-sm text-gray-400",
 
-        menu: () => "bg-base-100 my-1 mt-1 rounded",
+        menu: () => "bg-base-300 my-1 mt-1 rounded",
         option: ({ isFocused }) => "p-2" + " " + (isFocused ? "bg-base-200" : "bg-base-100"),
         noOptionsMessage: () => "my-3",
 
-        indicatorsContainer: () => "m-1",
+        indicatorsContainer: () => "absolute w-full flex justify-end h-11 pr-4",
         indicatorSeparator: () => "my-2 bg-gray-400",
 
         dropdownIndicator: () => "p-2",
         clearIndicator: () => "p-2",
 
-        valueContainer: () => "flex gap-1",
-        multiValue: () => "bg-base-300 text-base-content rounded-md",
-        multiValueLabel: () => "m-2 my-1",
+        valueContainer: () => "absolute flex-none gap-1 overflow-scroll",
+        multiValue: () => "bg-base-300 text-base-content rounded-md my-auto overflow-items-scroll",
+        multiValueLabel: () => "ml-2 my-1",
         multiValueRemove: () => "hover:bg-error px-2 rounded-md",
       }}
     />
