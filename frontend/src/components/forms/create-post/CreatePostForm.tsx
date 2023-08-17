3a6b73e4c54c9c434919fb04ba36f49e8caa8a5a
Maksim Mikhailov <max@mer.pw>
2023-08-17 16:58:06 +0300

    Fix submit button position in forms
    in CreatePostForm and CreateGroupForm

--- frontend/src/components/forms/create-post/CreatePostForm.tsx
+++ frontend/src/components/forms/create-post/CreatePostForm.tsx
@@ -131,26 +131,27 @@ const CreatePostForm: FC<{ categories: string[]; isAIEnabled: boolean; group?: G
 
             <div className={"flex flex-row justify-between gap-3"}>
               {group ? null : <PostPrivacy />}
-              <div className={"form-control justify-end"}>
-                <button type={"submit"} className={"btn gradient-accent-light rounded-full p-3"}>
-                  <svg
-                    xmlns={"http://www.w3.org/2000/svg"}
-                    fill={"none"}
-                    viewBox={"0 0 24 24"}
-                    strokeWidth={2}
-                    stroke={"currentColor"}
-                    className={"w-6 text-primary-content"}
-                  >
-                    <path
-                      strokeLinecap={"round"}
-                      strokeLinejoin={"round"}
-                      d={
-                        "M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                      }
-                    />
-                  </svg>
-                </button>
-              </div>
+              <button
+                type={"submit"}
+                className={"btn gradient-accent-light rounded-full p-3 ml-auto mt-auto"}
+              >
+                <svg
+                  xmlns={"http://www.w3.org/2000/svg"}
+                  fill={"none"}
+                  viewBox={"0 0 24 24"}
+                  strokeWidth={2}
+                  stroke={"currentColor"}
+                  className={"w-6 text-primary-content"}
+                >
+                  <path
+                    strokeLinecap={"round"}
+                    strokeLinejoin={"round"}
+                    d={
+                      "M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                    }
+                  />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
