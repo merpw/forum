df0bb4704f0e05262e8cc85f3369e5e9a87b753c
Maksim Mikhailov <max@mer.pw>
2023-08-17 16:49:00 +0300

    Fix Select component styles
    Add multiple lines support
    
    Fix bug with deselected button not working
    
    This reverts commit 4bd4cb3ade21c73d56131d8a3fff07c29fb1bf38.

--- frontend/src/components/Select.tsx
+++ frontend/src/components/Select.tsx
@@ -5,7 +5,6 @@ const Select: ReactSelect = (props) => {
     <ReactSelect
       {...props}
       unstyled={true}
-      className={""}
       styles={{
         control: (provided) => ({
           ...provided,
@@ -15,25 +14,25 @@ const Select: ReactSelect = (props) => {
       }}
       classNames={{
         control: ({ isFocused }) =>
-          "relative input " +
+          "input pr-0 h-fit" +
           " " +
           (isFocused ? "outline outline-offset-2 outline-base-content/20" : "text-gray-400"),
 
         placeholder: () => "text-sm text-gray-400",
 
-        menu: () => "bg-base-300 my-1 mt-1 rounded",
+        menu: () => "bg-base-100 my-1 mt-1 rounded",
         option: ({ isFocused }) => "p-2" + " " + (isFocused ? "bg-base-200" : "bg-base-100"),
         noOptionsMessage: () => "my-3",
 
-        indicatorsContainer: () => "absolute w-full flex justify-end h-11 pr-4",
+        indicatorsContainer: () => "m-1",
         indicatorSeparator: () => "my-2 bg-gray-400",
 
         dropdownIndicator: () => "p-2",
         clearIndicator: () => "p-2",
 
-        valueContainer: () => "absolute flex-none gap-1 overflow-scroll",
-        multiValue: () => "bg-base-300 text-base-content rounded-md my-auto overflow-items-scroll",
-        multiValueLabel: () => "ml-2 my-1",
+        valueContainer: () => "flex gap-1",
+        multiValue: () => "bg-base-300 text-base-content rounded-md",
+        multiValueLabel: () => "m-2 my-1",
         multiValueRemove: () => "hover:bg-error px-2 rounded-md",
       }}
     />
